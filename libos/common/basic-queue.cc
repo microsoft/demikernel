7cb6120a8851bf824c7f872cf9d1f8e421c37da0
irene <irene@iyzhang-desktop.corp.microsoft.com>
2018-08-13 16:23:13 -0700

    adding a new interface

---
+++ libos/common/basic-queue.cc
@@ -0,0 +1,127 @@
+// -*- mode: c++; c-file-style: "k&r"; c-basic-offset: 4 -*-
+/***********************************************************************
+ *
+ * common/basic-queue.cc
+ *   Basic queue implementation
+ *
+ * Copyright 2018 Irene Zhang  <irene.zhang@microsoft.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************/
+ 
+#include "common/basic-queue.h"
+#include <assert.h>
+
+namespace Zeus {
+    
+ssize_t
+BasicQueue::push(qtoken qt, struct sgarray &sga)
+{
+    size_t len = 0;
+    std::lock_guard<std::mutex> lock(qLock);
+    if (!waiting.empty()) {
+        qtoken pop = waiting.front();
+        waiting.pop_front();
+        auto it = pending.find(pop);
+        PendingRequest *req = it->second;
+        len = req->sga.copy(sga);
+	assert(len > 0);
+        req->cv.notify_all();
+        req->isDone = true;
+    } else {
+	for (int i = 0; i < sga.num_bufs; i++) {
+	    len += sga.bufs[i].len;
+	}
+        buffer.push_back(sga);
+    }
+    //fprintf(stderr, "[%x] pushing %lx size(%ld)", qd, qt, len);
+    return len;
+}
+        
+ssize_t
+BasicQueue::pop(qtoken qt, struct sgarray &sga) {
+    size_t len = 0;
+    std::lock_guard<std::mutex> lock(qLock);
+    // if we have a buffered push already
+    if (!buffer.empty()) {
+        len = sga.copy(buffer.front());
+        buffer.pop_front();
+    } else {
+        pending.insert(
+            std::make_pair(qt, new PendingRequest(sga)));
+        waiting.push_back(qt);
+        assert(pending.find(qt) != pending.end());
+    }
+    return len;
+}
+
+ssize_t
+BasicQueue::peek(struct sgarray &sga) {
+    size_t len = 0;
+    std::lock_guard<std::mutex> lock(qLock);
+    // if we have a buffered push already
+    if (!buffer.empty()) {
+        len = sga.copy(buffer.front());
+        buffer.pop_front();
+    }
+    return len;
+}
+
+ssize_t
+BasicQueue::wait(qtoken qt, struct sgarray &sga)
+{
+    std::unique_lock<std::mutex> lock(qLock);
+    lock.lock();
+    auto it = pending.find(qt);
+    assert(it != pending.end());
+    PendingRequest *req = it->second;
+    while (!req->isDone) {
+        req->cv.wait(lock);
+    }
+    size_t len = sga.copy(req->sga);
+    delete it->second;
+    lock.unlock();
+    delete req;
+    return len;
+}
+    
+ssize_t
+BasicQueue::poll(qtoken qt, struct sgarray &sga)
+{
+    size_t len = 0;
+    std::lock_guard<std::mutex> lock(qLock);
+    auto it = pending.find(qt);
+    assert(it != pending.end());
+    PendingRequest *req = it->second;
+
+    if (req->isDone) {
+        len = sga.copy(req->sga);
+        pending.erase(it);
+        delete req;
+	assert(len > 0);
+        return len;
+    } else {
+        return 0;
+    }
+
+}
+}// namespace Zeus
---
+++ libos/common/basic-queue.h
@@ -0,0 +1,102 @@
+// -*- mode: c++; c-file-style: "k&r"; c-basic-offset: 4 -*-
+/***********************************************************************
+ *
+ * common/queue.h
+ *   Basic queue
+ *
+ * Copyright 2018 Irene Zhang  <irene.zhang@microsoft.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************/
+ 
+#ifndef _BASIC_QUEUE_H_
+#define _BASIC_QUEUE_H_
+
+#include "include/io-queue.h"
+#include "common/queue.h"
+#include <unordered_map>
+#include <condition_variable>
+#include <mutex>
+#include <list>
+
+namespace Zeus {
+    
+class BasicQueue : public Queue
+{
+
+private:
+    struct PendingRequest
+    {
+        bool isDone;
+        sgarray &sga;
+        std::condition_variable cv;
+
+        PendingRequest(struct sgarray &sga) :
+            isDone(false),
+            sga(sga) { };
+        ~PendingRequest() { };
+    };
+    
+    // queued scatter gather arrays
+    std::unordered_map<qtoken, PendingRequest *> pending;
+    std::list<qtoken> waiting;
+    std::list<sgarray> buffer;
+    std::mutex qLock;
+    
+protected:
+    QueueType type;
+    int qd;
+    
+public:
+    BasicQueue() : type(BASIC_Q), qd(0) { };
+    BasicQueue(QueueType type, int qd) : type(type), qd(qd) { };
+    virtual ~BasicQueue() { };
+    virtual int GetQD() { return qd; };
+    virtual QueueType GetType() { return type; };
+    virtual void SetQD(int q) { qd = q; };
+    virtual void SetType(QueueType t) { type = t; };
+
+    //int queue();
+    // network control plane functions
+    virtual int socket(int domain, int type, int protocol) { return 0; };
+    virtual int getsockname(sockaddr *saddr, socklen_t *size) { return 0; };
+    virtual int listen(int backlog) { return 0; };
+    virtual int bind(struct sockaddr *saddr, socklen_t size) { return 0; };
+    virtual int accept(struct sockaddr *saddr, socklen_t *size) { return 0; };
+    virtual int connect(struct sockaddr *saddr, socklen_t size) { return 0; };
+    virtual int close() { return 0; };
+          
+    // file control plane functions
+    virtual int open(const char *pathname, int flags) { return 0; };
+    virtual int open(const char *pathname, int flags, mode_t mode) { return 0; };
+    virtual int creat(const char *pathname, mode_t mode) {return 0; };
+
+    // data plane functions
+    virtual ssize_t push(qtoken qt, struct sgarray &sga);
+    virtual ssize_t pop(qtoken qt, struct sgarray &sga);
+    virtual ssize_t peek(struct sgarray &sga);
+    virtual ssize_t wait(qtoken qt, struct sgarray &sga);
+    virtual ssize_t poll(qtoken qt, struct sgarray &sga);
+};
+
+} // namespace Zeus
+#endif /* _BASIC_QUEUE_H_ */
