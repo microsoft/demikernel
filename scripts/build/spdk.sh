81618f9b57eae6eba0d8aebd92ce8cce525393d1
Michael Lowell Roberts <mirobert@microsoft.com>
2018-11-02 14:14:10 -0700

    updated setup script.

---
+++ scripts/build/spdk.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+repo_root=$(git rev-parse --show-toplevel)
+spdk_root=${repo_root}/submodules/spdk
+
+pushd ${spdk_root} >> /dev/null
+trap 'popd >> /dev/null' EXIT
+
+./configure
+make
+./test/unit/unittest.sh
