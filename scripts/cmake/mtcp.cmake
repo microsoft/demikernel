97369c3da873c44e8710579c0d0fe1c0be151858
Michael Lowell Roberts <mirobert@microsoft.com>
2018-12-03 14:18:53 -0800

    ported changes to `dpdk.cmake` to `mtcp.cmake`.

--- scripts/cmake/mtcp.cmake
+++ scripts/cmake/mtcp.cmake
@@ -20,14 +20,19 @@ ExternalProject_Add(mtcp_dpdk
   BUILD_COMMAND make -C ${MTCP_DPDK_SOURCE_DIR} T=${DPDK_TARGET}
   INSTALL_COMMAND make -C ${MTCP_DPDK_SOURCE_DIR} install T=${DPDK_TARGET} DESTDIR=${MTCP_DPDK_INSTALL_DIR}
 )
+
 if(DPDK_USE_MELLANOX_PMD)
   set(MTCP_DPDK_CONFIG_RTE_LIBRTR_MLX5_PMD y)
-  set(MTCP_DPDK_MLX5_PMD_LINK_FLAGS -Wl,-lrte_pmd_mlx5)
 else(DPDK_USE_MELLANOX_PMD)
   set(MTCP_DPDK_CONFIG_RTE_LIBRTR_MLX5_PMD n)
 endif(DPDK_USE_MELLANOX_PMD)
 set(MTCP_DPDK_CONFIG_COMMON_BASE ${MTCP_DPDK_SOURCE_DIR}/config/common_base)
 configure_file(${MTCP_DPDK_CONFIG_COMMON_BASE}.in ${MTCP_DPDK_CONFIG_COMMON_BASE})
+
+# mTCP has hacked the DPDK build to divulge the flags it generated for
+# compilation and linking.
+set(MTCP_DPDK_CFLAGS_FILE ${MTCP_DPDK_SOURCE_DIR}/${DPDK_TARGET}/include/cflags.txt)
+set(MTCP_DPDK_LDFLAGS_FILE ${MTCP_DPDK_SOURCE_DIR}/${DPDK_TARGET}/lib/ldflags.txt)
 
 # mTCP
 set(MTCP_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/submodules/mtcp)
@@ -47,8 +52,8 @@ function(target_add_mtcp TARGET)
   add_dependencies(${TARGET} mtcp mtcp_dpdk)
   # mTCP's DPDK flags
   set_target_properties(${TARGET} PROPERTIES
-    COMPILE_FLAGS "-march=native -DRTE_MACHINE_CPUFLAG_SSE -DRTE_MACHINE_CPUFLAG_SSE2 -DRTE_MACHINE_CPUFLAG_SSE3 -DRTE_MACHINE_CPUFLAG_SSSE3 -DRTE_MACHINE_CPUFLAG_SSE4_1 -DRTE_MACHINE_CPUFLAG_SSE4_2"
-    LINK_FLAGS "-L${MTCP_DPDK_LIB_DIR} -Wl,-lrte_pipeline -Wl,-lrte_table -Wl,-lrte_port -Wl,-lrte_pdump -Wl,-lrte_distributor -Wl,-lrte_ip_frag -Wl,-lrte_gro -Wl,-lrte_meter -Wl,-lrte_sched -Wl,-lrte_lpm -Wl,--whole-archive -Wl,-lrte_acl -Wl,--no-whole-archive -Wl,-lrte_jobstats -Wl,-lrte_metrics -Wl,-lrte_bitratestats -Wl,-lrte_latencystats -Wl,-lrte_power -Wl,-lrte_timer -Wl,-lrte_efd -Wl,-lrte_cfgfile -Wl,--whole-archive -Wl,-lrte_hash -Wl,-lrte_vhost -Wl,-lrte_kvargs -Wl,-lrte_mbuf -Wl,-lrte_net -Wl,-lrte_ethdev -Wl,-lrte_cryptodev -Wl,-lrte_eventdev -Wl,-lrte_mempool -Wl,-lrte_mempool_ring -Wl,-lrte_ring -Wl,-lrte_eal -Wl,-lrte_cmdline -Wl,-lrte_reorder -Wl,-lrte_kni -Wl,-lrte_mempool_stack -Wl,-lrte_pmd_af_packet -Wl,-lrte_pmd_ark -Wl,-lrte_pmd_avp -Wl,-lrte_pmd_bnxt -Wl,-lrte_pmd_bond -Wl,-lrte_pmd_cxgbe -Wl,-lrte_pmd_e1000 -Wl,-lrte_pmd_ena -Wl,-lrte_pmd_enic -Wl,-lrte_pmd_fm10k -Wl,-lrte_pmd_failsafe -Wl,-lrte_pmd_i40e -Wl,-lrte_pmd_ixgbe -Wl,-lrte_pmd_kni -Wl,-lrte_pmd_lio ${MTCP_DPDK_MLX5_PMD_LINK_FLAGS} -Wl,-libverbs -Wl,-lrte_pmd_nfp -Wl,-lrte_pmd_null -Wl,-lrte_pmd_qede -Wl,-lrte_pmd_ring -Wl,-lrte_pmd_sfc_efx -Wl,-lrte_pmd_tap -Wl,-lrte_pmd_thunderx_nicvf -Wl,-lrte_pmd_virtio -Wl,-lrte_pmd_vhost -Wl,-lrte_pmd_vmxnet3_uio -Wl,-lrte_pmd_null_crypto -Wl,-lrte_pmd_crypto_scheduler -Wl,-lrte_pmd_skeleton_event -Wl,-lrte_pmd_sw_event -Wl,-lrte_pmd_octeontx_ssovf -Wl,--no-whole-archive -Wl,-lrt -Wl,-lm -Wl,-lnuma -Wl,-ldl"
+    COMPILE_FLAGS @${MTCP_DPDK_CFLAGS_FILE}
+    LINK_FLAGS @${MTCP_DPDK_LDFLAGS_FILE}
   )
 endfunction(target_add_mtcp)
 
