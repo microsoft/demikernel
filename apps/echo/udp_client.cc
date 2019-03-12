ecc57ab6c80d7fb34723e5c2aa4ff3a6ab2612e3
Michael Lowell Roberts <mirobert@microsoft.com>
2019-03-12 11:30:39 -0700

    client-side metrics & release mode optimizations

--- apps/echo/udp_client.cc
+++ apps/echo/udp_client.cc
@@ -64,6 +64,11 @@ int main(int argc, char *argv[])
 
     DMTR_OK(dmtr_init(argc, argv));
 
+    dmtr_timer_t *pop_timer = NULL;
+    DMTR_OK(dmtr_newtimer(&pop_timer, "pop"));
+    dmtr_timer_t *push_timer = NULL;
+    DMTR_OK(dmtr_newtimer(&push_timer, "push"));
+
     int qd = 0;
     DMTR_OK(dmtr_socket(&qd, AF_INET, SOCK_DGRAM, 0));
     printf("client qd:\t%d\n", qd);
@@ -88,18 +93,22 @@ int main(int argc, char *argv[])
 
     for (size_t i = 0; i < ITERATION_COUNT; i++) {
         dmtr_qtoken_t qt;
+        DMTR_OK(dmtr_starttimer(pop_timer));
         DMTR_OK(dmtr_push(&qt, qd, &sga));
         DMTR_OK(dmtr_wait(NULL, qt));
+        DMTR_OK(dmtr_stoptimer(pop_timer));
         DMTR_OK(dmtr_drop(qt));
         //fprintf(stderr, "send complete.\n");
 
         dmtr_qresult_t qr = {};
+        DMTR_OK(dmtr_starttimer(push_timer));
         DMTR_OK(dmtr_pop(&qt, qd));
         DMTR_OK(dmtr_wait(&qr, qt));
+        DMTR_OK(dmtr_stoptimer(push_timer));
         DMTR_OK(dmtr_drop(qt));
-        DMTR_TRUE(EPERM, DMTR_OPC_POP == qr.qr_opcode);
-        DMTR_TRUE(EPERM, qr.qr_value.sga.sga_numsegs == 1);
-        DMTR_TRUE(EPERM, reinterpret_cast<uint8_t *>(qr.qr_value.sga.sga_segs[0].sgaseg_buf)[0] == FILL_CHAR);
+        assert(DMTR_OPC_POP == qr.qr_opcode);
+        assert(qr.qr_value.sga.sga_numsegs == 1);
+        assert(reinterpret_cast<uint8_t *>(qr.qr_value.sga.sga_segs[0].sgaseg_buf)[0] == FILL_CHAR);
 
         //fprintf(stderr, "[%lu] client: rcvd\t%s\tbuf size:\t%d\n", i, reinterpret_cast<char *>(qr.qr_value.sga.sga_segs[0].sgaseg_buf), qr.qr_value.sga.sga_segs[0].sgaseg_len);
         free(qr.qr_value.sga.sga_buf);
