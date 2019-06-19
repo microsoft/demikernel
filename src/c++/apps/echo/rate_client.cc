e61b35b8c847271d89fb4f3d07f39b103443656d
Max Demoulin <maxdml@prometheus2.prometheus>
2019-06-19 21:32:20 +0000

    move some log macros to common.hh

--- src/c++/apps/echo/rate_client.cc
+++ src/c++/apps/echo/rate_client.cc
@@ -25,74 +25,9 @@
 #include <dmtr/wait.h>
 
 /*****************************************************************
- *********************** LOGGING MACROS   ************************
- *****************************************************************/
-
-/* Enable debug statements  */
-//#define LOG_DEBUG
-
-/* Where command-line output gets printed to  */
-#define LOG_FD stderr
-
-/* For coloring log output  */
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-#define ANSI_COLOR_PURPLE   "\x1b[35m"
-
-/* General logging function which can be filled in with arguments, color, etc. */
-#define log_at_level(lvl_label, color, fd, fmt, ...)\
-        fprintf(fd, "" color "%07.03f:%s:%d:%s(): " lvl_label ": " fmt ANSI_COLOR_RESET "\n", \
-                ((std::chrono::duration<double>)(std::chrono::system_clock::now() - start_time)).count(), \
-                __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-
-/* Debug statements are replaced with nothing if LOG_DEBUG is false  */
-#ifdef LOG_DEBUG
-#define log_debug(fmt, ...)\
-    log_at_level("DEBUG", ANSI_COLOR_RESET, LOG_FD, fmt, ##__VA_ARGS__)
-#else
-#define log_debug(...)
-#endif
-
-#ifdef PRINT_RESPONSES
-#define print_response(fmt, ...)\
-    fprintf(LOG_FD, fmt "\n", ##__VA_ARGS__);
-#else
-#define print_response(...)
-#endif
-
-#define log_info(fmt, ...)\
-    log_at_level("INFO", ANSI_COLOR_GREEN, LOG_FD, fmt, ##__VA_ARGS__)
-#define log_error(fmt, ...)\
-    log_at_level("ERROR", ANSI_COLOR_RED, LOG_FD, fmt, ##__VA_ARGS__)
-#define log_warn(fmt, ...)\
-    log_at_level("WARN", ANSI_COLOR_YELLOW, LOG_FD, fmt, ##__VA_ARGS__)
-
-#ifdef PRINT_REQUEST_ERRORS
-#define print_request_error(fmt, ...)\
-    log_error(fmt, ##__VA_ARGS__);
-#else
-#define print_request_error(...)
-#endif
-
-/**
- * Simple macro to replace perror with out log format
- */
-#define log_perror(fmt, ...) \
-    log_error(fmt ": %s", ##__VA_ARGS__, strerror(errno))
-
-/**
- * Same as above, but to be used only for request-based errors
- */
-#define perror_request(fmt, ...) \
-    print_request_error(fmt ": %s", ##__VA_ARGS__, strerror(errno))
-
-/*****************************************************************
  *********************** TIME VARIABLES **************************
  *****************************************************************/
 
-auto start_time = std::chrono::system_clock::now();
 using hr_clock = std::chrono::high_resolution_clock;
 
 /* Returns the number of nanoseconds since the epoch for a given high-res time point */
