60c272a4c1b4c16e3a8134a8a191b75c13717983
maxdml <t-hedemo@microsoft.com>
2019-06-28 01:57:48 +0000

    now maintain a success_poll_latency object per thread (rather than having one shared by the whole VAS)

--- src/c++/libos/common/wait.cc
+++ src/c++/libos/common/wait.cc
@@ -9,12 +9,14 @@
 #include <dmtr/fail.h>
 #include <dmtr/latency.h>
 #include <dmtr/libos.h>
+#include <pthread.h>
+#include <unordered_map>
 
 #define DMTR_PROFILE 1
 
 #if DMTR_PROFILE
 typedef std::unique_ptr<dmtr_latency_t, std::function<void(dmtr_latency_t *)>> latency_ptr_type;
-static latency_ptr_type success_poll_latency;
+static std::unordered_map<pthread_t, latency_ptr_type> success_poll_latencies;
 #endif
 
 int dmtr_wait(dmtr_qresult_t *qr_out, dmtr_qtoken_t qt) {
@@ -28,13 +30,20 @@ int dmtr_wait(dmtr_qresult_t *qr_out, dmtr_qtoken_t qt) {
 
 int dmtr_wait_any(dmtr_qresult_t *qr_out, int *ready_offset, dmtr_qtoken_t qts[], int num_qts) {
 #if DMTR_PROFILE
-    if (NULL == success_poll_latency) {
-        dmtr_latency_t *l;
+    pthread_t me = pthread_self();
+    dmtr_latency_t *l;
+    auto it = success_poll_latencies.find(me);
+    if (it == success_poll_latencies.end()) {
         DMTR_OK(dmtr_new_latency(&l, "dmtr success poll"));
-        success_poll_latency = latency_ptr_type(l, [](dmtr_latency_t *latency) {
+        latency_ptr_type success_poll_latency = latency_ptr_type(l, [](dmtr_latency_t *latency) {
             dmtr_dump_latency(stderr, latency);
             dmtr_delete_latency(&latency);
         });
+        success_poll_latencies.insert(
+            std::pair<pthread_t, latency_ptr_type>(me, std::move(success_poll_latency))
+        );
+    } else {
+        l = it->second.get();
     }
 #endif
     while (1) {
@@ -48,7 +57,7 @@ int dmtr_wait_any(dmtr_qresult_t *qr_out, int *ready_offset, dmtr_qtoken_t qts[]
                     DMTR_OK(dmtr_drop(qts[i]));
 #if DMTR_PROFILE
                     auto dt = (boost::chrono::steady_clock::now() - t0);
-                    DMTR_OK(dmtr_record_latency(success_poll_latency.get(), dt.count()));
+                    DMTR_OK(dmtr_record_latency(l, dt.count()));
 #endif
                     if (ready_offset != NULL)
                         *ready_offset = i;
