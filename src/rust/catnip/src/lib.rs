148dc297651fc16e5c56ff33d769705b0ee0c04b
Michael Lowell Roberts <mirobert@microsoft.com>
2019-07-02 15:50:56 -0700

    Revert "use mimalloc as global allocator (faster, supposedly)"
    This reverts commit 11fcadc1bf89b2e57bed8df73c959adfa9c104e9.

--- src/rust/catnip/src/lib.rs
+++ src/rust/catnip/src/lib.rs
@@ -42,8 +42,3 @@ pub use engine::Engine;
 pub use io::IoVec;
 pub use options::Options;
 pub use runtime::Runtime;
-
-use mimalloc::MiMalloc;
-
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
