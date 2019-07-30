6091cc1c25ef22f2441154da0a56584756b85276
maxdml <t-hedemo@microsoft.com>
2019-07-30 02:59:29 +0000

    lwip echo programs now pass directly their argc/argv to dmtr_init

--- src/c++/apps/echo/dmtr_tcp_server.cc
+++ src/c++/apps/echo/dmtr_tcp_server.cc
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
     }
     saddr.sin_port = htons(port);
 
-    DMTR_OK(dmtr_init(dmtr_argc, dmtr_argv));
+    DMTR_OK(dmtr_init(argc, argv));
     DMTR_OK(dmtr_new_latency(&pop_latency, "pop server"));
     DMTR_OK(dmtr_new_latency(&push_latency, "push server"));
     DMTR_OK(dmtr_new_latency(&push_wait_latency, "push wait server"));
