2e4f69ee31da25f2ee7e855a85643863e15cc10f
irene <irene@iyzhang-desktop.corp.microsoft.com>
2019-09-04 13:56:00 -0700

    finished API, removed from main readme and made some arguments in include files consistent

--- include/dmtr/libos.h
+++ include/dmtr/libos.h
@@ -31,12 +31,12 @@ DMTR_EXPORT int dmtr_is_qd_valid(int *flag_out, int qd);
 
 DMTR_EXPORT int dmtr_push(
     dmtr_qtoken_t *qtok_out, int qd, const dmtr_sgarray_t *sga);
-DMTR_EXPORT int dmtr_pop(dmtr_qtoken_t *qt_out, int qd);
-DMTR_EXPORT int dmtr_pop2(dmtr_qtoken_t *qt_out, int qd, size_t count);
+DMTR_EXPORT int dmtr_pop(dmtr_qtoken_t *qtok_out, int qd);
+DMTR_EXPORT int dmtr_pop2(dmtr_qtoken_t *qtok_out, int qd, size_t count);
 DMTR_EXPORT int dmtr_lseek(int qd, off_t offset, int whence);
     
-DMTR_EXPORT int dmtr_poll(dmtr_qresult_t *qr_out, dmtr_qtoken_t qt);
-DMTR_EXPORT int dmtr_drop(dmtr_qtoken_t qt);
+DMTR_EXPORT int dmtr_poll(dmtr_qresult_t *qr_out, dmtr_qtoken_t qtok);
+DMTR_EXPORT int dmtr_drop(dmtr_qtoken_t qtok);
 
 #ifdef __cplusplus
 }
