f46b85d91ea3f91c59ac6b2e24ab0ec9a5a5d29d
Michael Lowell Roberts <mirobert@microsoft.com>
2019-10-01 20:43:00 +0000

    azure apparently needs `modprobe mlx4_ib` for DPDK support now.

--- scripts/setup/dpdk.sh
+++ scripts/setup/dpdk.sh
@@ -8,4 +8,5 @@
 echo 1024 | sudo tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages
 mkdir /mnt/huge || true
 mount -t hugetlbfs nodev /mnt/huge
-modprobe -a ib_uverbs
+modprobe ib_uverbs
+modprobe mlx4_ib
