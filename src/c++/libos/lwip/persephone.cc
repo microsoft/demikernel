95aec3c4f56fda1074d6d1d4b394904fde3447dc
maxdml <maxdml@seas.upenn.edu>
2019-10-30 15:39:33 -0400

    remove obsolete port setting through opt

--- src/c++/libos/lwip/persephone.cc
+++ src/c++/libos/lwip/persephone.cc
@@ -13,18 +13,6 @@
 int PspServiceUnit::init(int argc, char *argv[]) {
     if (my_type == dmtr::io_queue::category_id::NETWORK_Q) { //maybe prefix
         DMTR_OK(dmtr::lwip_queue::init_dpdk(argc, argv));
-        uint16_t port;
-        namespace po = boost::program_options;
-        po::options_description desc{"LWIP libos options"};
-        desc.add_options()
-            ("port", po::value<uint16_t>(&port)->default_value(6789),
-             "Port for lwip queues to listen to");
-        po::variables_map vm;
-        po::parsed_options parsed =
-            po::command_line_parser(argc, argv).options(desc).allow_unregistered().run();
-        po::store(parsed, vm);
-        po::notify(vm);
-        dmtr::lwip_queue::set_app_port(port);
     }
 
     ioqapi.register_queue_ctor(dmtr::io_queue::MEMORY_Q, dmtr::memory_queue::new_object);
