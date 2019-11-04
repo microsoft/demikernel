9c928cf2192b4a5279a720addb42fec9f0ef7d86
maxdml <maxdml@seas.upenn.edu>
2019-11-03 23:15:18 -0500

    have PspServiceUnit parse arguments for the log directory at init()

--- src/c++/libos/lwip/persephone.cc
+++ src/c++/libos/lwip/persephone.cc
@@ -9,10 +9,25 @@
 #include <dmtr/libos/memory_queue.hh>
 #include "lwip_queue.hh"
 
-
 int PspServiceUnit::init(int argc, char *argv[]) {
+
+
     if (my_type == dmtr::io_queue::category_id::NETWORK_Q) { //maybe prefix
         DMTR_OK(dmtr::lwip_queue::init_dpdk(argc, argv));
+    }
+
+    if (argc > 0) {
+        std::string log_directory;
+        namespace po = boost::program_options;
+        po::options_description desc{"IO queue API options"};
+        desc.add_options()
+            ("log-dir,L", po::value<std::string>(&log_directory)->default_value("./"), "Log directory");
+        po::variables_map vm;
+        po::parsed_options parsed =
+            po::command_line_parser(argc, argv).options(desc).allow_unregistered().run();
+        po::store(parsed, vm);
+        po::notify(vm);
+        dmtr_log_directory = log_directory;
     }
 
     ioqapi.register_queue_ctor(dmtr::io_queue::MEMORY_Q, dmtr::memory_queue::new_object);
