292243819da8277f19ec44f63b7c3d739eef7dda
maxdml <maxdml@seas.upenn.edu>
2019-11-25 13:21:37 -0500

    add pipeline to http cl client

--- src/c++/apps/echo/dmtr_cl_client.cc
+++ src/c++/apps/echo/dmtr_cl_client.cc
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #include <dmtr/libos/io_queue.hh>
 
@@ -19,9 +20,68 @@ void sig_handler(int signo) {
     terminate = true;
 }
 
+void pin_thread(pthread_t thread, u_int16_t cpu) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(cpu, &cpuset);
+
+    int rtn = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    if (rtn != 0) {
+        fprintf(stderr, "could not pin thread: %s\n", strerror(errno));
+    }
+}
+
+std::unique_ptr<ClientRequest> send_request(PspServiceUnit &su, int qfd, std::string request_str, int id) {
+    dmtr_sgarray_t sga;
+    std::unique_ptr<ClientRequest> cr(format_request(id, request_str, ip));
+    sga.sga_numsegs = 1;
+    sga.sga_segs[0].sgaseg_len = cr->req_size;
+    sga.sga_segs[0].sgaseg_buf = static_cast<void *>(cr->req);
+    /* Schedule and send the request */
+#ifdef DMTR_TRACE
+    cr->sending = take_time();
+#endif
+    dmtr_qtoken_t token;
+    su.ioqapi.push(token, qfd, sga);
+#ifdef DMTR_TRACE
+    cr->push_token = token;
+#endif
+    dmtr_qresult_t qr;
+    while (su.wait(&qr, token) == EAGAIN) {
+        if (terminate) { break; }
+    } //XXX should we check for severed connection here?
+    assert(DMTR_OPC_PUSH == qr.qr_opcode);
+#ifdef DMTR_TRACE
+    cr->reading = take_time();
+#endif
+    return std::move(cr);
+}
+
+int recv_request(PspServiceUnit &su, int qfd, ClientRequest *cr) {
+    dmtr_qtoken_t token;
+    /* Wait for an answer */
+    su.ioqapi.pop(token, qfd);
+#ifdef DMTR_TRACE
+    cr.pop_token = token;
+#endif
+    dmtr_qresult_t qr;
+    int wait_rtn;
+    while ((wait_rtn = su.wait(&qr, token)) == EAGAIN) {
+        if (terminate) { return ETIME; }
+    }
+    assert(DMTR_OPC_POP == qr.qr_opcode);
+    assert(qr.qr_value.sga.sga_numsegs == 1);
+#ifdef DMTR_TRACE
+    cr.completed = take_time();
+#endif
+    dmtr_free_mbuf(&qr.qr_value.sga);
+    return wait_rtn;
+}
+
 int main (int argc, char *argv[]) {
     /* Parse options */
     int duration;
+    uint16_t pipeline;
     std::string uri_list, label, log_dir, remote_host;
     namespace po = boost::program_options;
     po::options_description desc{"Rate client options"};
@@ -29,7 +89,8 @@ int main (int argc, char *argv[]) {
         ("duration,d", po::value<int>(&duration)->default_value(10), "Runtime duration")
         ("label,l", po::value<std::string>(&label)->default_value("rate_client"), "experiment label")
         ("log-dir,L", po::value<std::string>(&log_dir)->default_value("./"), "Log directory")
-        ("uris-list,f", po::value<std::string>(&uri_list)->required(), "List of URIs to request");
+        ("uri-list,f", po::value<std::string>(&uri_list)->required(), "List of URIs to request")
+        ("pipeline,p", po::value<uint16_t>(&pipeline)->default_value(1), "Number of requests to keep in flight");
     parse_args(argc, argv, false, desc);
 
     log_info(
@@ -49,6 +110,8 @@ int main (int argc, char *argv[]) {
 
     /* Init Persephone ServiceUnit */
     PspServiceUnit su(0, dmtr::io_queue::category_id::NETWORK_Q, argc, argv);
+
+    pin_thread(pthread_self(), 4);
 
     /* Configure socket */
     struct sockaddr_in saddr = {};
@@ -71,58 +134,54 @@ int main (int argc, char *argv[]) {
     log_info("Running for %lu", duration_tp.count());
 
     std::vector<std::unique_ptr<ClientRequest> > requests;
-    requests.reserve(100000); //XXX
+    requests.reserve(1000000); //XXX
+    int resp_idx = 0;
+
+    uint32_t sent_requests = 0;
+    uint32_t rcv_requests = 0;
+    for (int i = 0; i < pipeline - 1; i++) {
+        std::string request_str = requests_str[sent_requests % requests_str.size()];
+        requests.push_back(std::move(send_request(su, qfd, request_str, sent_requests)));
+        sent_requests++;
+    }
+
+    //FIXME this assumes that we always receive the request we just sent $pipeline ago
+    //Plus we will always ignore $pipeline - & responses
     dmtr_qtoken_t token;
     dmtr_sgarray_t sga;
     dmtr_qresult_t qr;
-    uint32_t sent_requests = 0;
     hr_clock::time_point start_time = take_time();
     while (take_time() - start_time < duration_tp) {
         /* Pick a request, craft it, prepare the SGA */
         std::string request_str = requests_str[sent_requests % requests_str.size()];
-        std::unique_ptr<ClientRequest> cr(format_request(sent_requests, request_str, ip));
+        requests.push_back(std::move(send_request(su, qfd, request_str, sent_requests)));
+        sent_requests++;
 
-        sga.sga_numsegs = 1;
-        sga.sga_segs[0].sgaseg_len = cr->req_size;
-        sga.sga_segs[0].sgaseg_buf = static_cast<void *>(cr->req);
-        /* Schedule and send the request */
-#ifdef DMTR_TRACE
-        cr->sending = take_time();
-#endif
-        DMTR_OK(su.ioqapi.push(token, qfd, sga));
-#ifdef DMTR_TRACE
-        cr->push_token = token;
-#endif
-        while (su.wait(&qr, token) == EAGAIN) {
-            if (terminate) { break; }
-        } //XXX should we check for severed connection here?
-        assert(DMTR_OPC_PUSH == qr.qr_opcode);
-#ifdef DMTR_TRACE
-        cr->reading = take_time();
-#endif
-        /* Wait for an answer */
-        DMTR_OK(su.ioqapi.pop(token, qfd));
-#ifdef DMTR_TRACE
-        cr->pop_token = token;
-#endif
-        int wait_rtn;
-        while ((wait_rtn = su.wait(&qr, token)) == EAGAIN) {
-            if (terminate) { break; }
-        }
+        int wait_rtn = recv_request(su, qfd, requests[resp_idx++].get());
         if (wait_rtn == ECONNABORTED || wait_rtn == ECONNRESET) {
             break;
         }
-        assert(DMTR_OPC_POP == qr.qr_opcode);
-        assert(qr.qr_value.sga.sga_numsegs == 1);
-#ifdef DMTR_TRACE
-        cr->completed = take_time();
-#endif
-        requests.push_back(std::move(cr));
-        free(qr.qr_value.sga.sga_buf);
-        sent_requests++;
+        if (wait_rtn == ETIME) {
+            break;
+        }
+        rcv_requests++;
+    }
+
+    /* Now listen for the number of pending requests */
+    for (uint16_t i = 0; i < pipeline - 1; ++i) {
+        int wait_rtn = recv_request(su, qfd, requests[resp_idx++].get());
+        if (wait_rtn == ECONNABORTED || wait_rtn == ECONNRESET) {
+            break;
+        }
+        if (wait_rtn == ETIME) {
+            break;
+        }
+        rcv_requests++;
     }
 
     DMTR_OK(su.ioqapi.close(qfd));
+    log_info("Sent: %d, Received: %d (missing: %d) ",
+             sent_requests, rcv_requests, sent_requests - rcv_requests);
 
 #ifdef DMTR_TRACE
     FILE *f = fopen(generate_log_file_path(log_dir, label, "traces").c_str(), "w");
