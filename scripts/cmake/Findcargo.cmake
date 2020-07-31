69b047add66c196d152660449099b17d452eb7d5
Irene Zhang <irene@prometheus8.prometheus>
2020-07-30 18:01:01 -0700

    adding a flag to skip rust checks

--- scripts/cmake/Findcargo.cmake
+++ scripts/cmake/Findcargo.cmake
@@ -4,10 +4,11 @@
 if(NOT FINDCARGO_DOT_CMAKE_INCLUDED)
 set(FINDCARGO_DOT_CMAKE_INCLUDED YES)
 
+if(NOT SKIP_RUST)
 include(FindPackageHandleStandardArgs)
 
 find_program(CARGO_EXECUTABLE cargo)
 find_package_handle_standard_args(cargo DEFAULT_MSG CARGO_EXECUTABLE)
 mark_as_advanced(CARGO_EXECUTABLE)
-
+endif(NOT SKIP_RUST)
 endif(NOT FINDCARGO_DOT_CMAKE_INCLUDED)
