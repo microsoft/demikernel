c964d4e97f9a0668adecab96703cbc90eb238e73
Sujay Jayakar <sujayakar314@gmail.com>
2020-09-22 17:00:13 -0700

    Continue hacking on Pages/Slabs

--- src/rust/catnip/src/collections/async_map.rs
+++ src/rust/catnip/src/collections/async_map.rs
@@ -4,76 +4,118 @@ use std::hash::Hash;
 use std::pin::Pin;
 use std::borrow::Borrow;
 use std::future::Future;
+use unicycle::pin_slab::PinSlab;
+use pin_project::pin_project;
+use futures::task::AtomicWaker;
 
-pub struct FutureMap<K: Clone + Hash + Eq + Unpin, F: ?Sized> {
-    map: HashMap<K, Box<F>>,
+
+// 1) Allocate a wait slot for a (parent_key, usize) pair
+// 1a) The usize may just be zero or it can range up and down some contiguous N
+// 2) Stash a waker for a particular parent
+// 3) Get a waker for a particular (parent_key, usize) pair -> single pointer, no allocation
+// 4) Wake this pointer
+// 5) Find all of the woken `usize`s under a parent and repeat.
+
+// Do we want an allocation per parent key? That's probably okay.
+// Do we want to statically bound the number of children under a node? That's also probably okay.
+
+#[pin_project]
+struct FuturePair<K, F> {
+    key: K,
+
+    #[pin]
+    future: F,
 }
 
-impl<K: Clone + Hash + Eq + Unpin, F: Future + ?Sized> FutureMap<K, F> {
+#[pin_project]
+pub struct FutureMap<K: Clone + Hash + Eq + Unpin, F: Future> {
+    parent: AtomicWaker,
+
+    futures: PinSlab<FuturePair<K, F>>,
+    map: HashMap<K, usize>,
+}
+
+impl<K: Clone + Hash + Eq + Unpin, F: Future> FutureMap<K, F> {
     pub fn new() -> Self {
         Self {
+            parent: AtomicWaker::new(),
+            futures: PinSlab::new(),
             map: HashMap::new(),
         }
     }
 
-    pub fn poll(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<(K, F::Output)> {
-        let self_ = self.get_mut();
-        let mut to_remove = None;
+    pub fn poll(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<(K, F::Output)> {
+        let self_ = self.as_mut().project();
+        self_.parent.register(ctx.waker());
 
-        for (k, v) in self_.map.iter_mut() {
-            let f: Pin<&mut F> = unsafe { Pin::new_unchecked(&mut *v) };
-            match Future::poll(f, ctx) {
+        for &ix in self_.map.values() {
+            let pair = match self_.futures.get_pin_mut(ix) {
+                None => continue,
+                Some(p) => p,
+            };
+            let pair_ = pair.project();
+            let output = match Future::poll(pair_.future, ctx) {
+                Poll::Ready(o) => o,
                 Poll::Pending => continue,
-                Poll::Ready(val) => {
-                    to_remove = Some((k.clone(), val));
-                },
-            }
+            };
+            let key = pair_.key.clone();
+            assert_eq!(self_.map.remove(&key), Some(ix));
+            assert!(self_.futures.remove(ix));
+
+            return Poll::Ready((key, output));
         }
-        if let Some((k, val)) = to_remove {
-            self_.map.remove(&k);
-            Poll::Ready((k, val))
-        } else {
-            Poll::Pending
-        }
+
+        Poll::Pending
     }
 }
 
-impl<K: Clone + Hash + Eq + Unpin, F> FutureMap<K, F> {
+impl<K: Clone + Hash + Eq + Unpin, F: Future> FutureMap<K, F> {
     pub fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
         where Q: Hash + Eq, K: Borrow<Q>
     {
         self.map.contains_key(key)
     }
 
-    /// NB: On collision, this returns the newly proposed key, not the existing one.
-    pub fn insert(&mut self, key: K, val: F) -> Option<(K, F)> {
+    /// NB: On collision, this returns the newly proposed value, not the existing one.
+    pub fn insert(&mut self, key: K, future: F) -> Option<(K, F)> {
         if self.map.contains_key(&key) {
-            return Some((key, val));
+            return Some((key, future));
         }
-        self.map.insert(key, Box::new(val));
+        let pair = FuturePair {
+            key: key.clone(),
+            future,
+        };
+        let ix = self.futures.insert(pair);
+        self.map.insert(key, ix);
+        self.parent.wake();
+
         None
     }
 
     pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&F>
         where Q: Hash + Eq, K: Borrow<Q>
     {
-        Some(&self.map.get(key)?.as_ref())
+        let &ix = self.map.get(key)?;
+        Some(&self.futures.get(ix).expect("Slab and map inconsistent").future)
     }
 
     pub fn get_pin_mut<Q: ?Sized>(&mut self, key: &Q) -> Option<Pin<&mut F>>
         where Q: Hash + Eq, K: Borrow<Q>
     {
-        let f: &mut F = &mut *self.map.get_mut(key)?;
-        Some(unsafe { Pin::new_unchecked(f) })
+        let &ix = self.map.get(key)?;
+        let pair = self.futures.get_pin_mut(ix)?;
+        Some(pair.project().future)
     }
 
     pub fn remove<Q: ?Sized>(&mut self, key: &Q) -> bool
         where Q: Hash + Eq, K: Borrow<Q>
     {
-        match self.map.remove(key) {
-            Some(..) => true,
-            None => false,
-        }
+        let ix = match self.map.remove(key) {
+            Some(ix) => ix,
+            None => return false,
+        };
+        assert!(self.futures.remove(ix));
+        true
     }
 
     pub fn len(&self) -> usize {
--- src/rust/catnip/src/collections/async_slab.rs
+++ src/rust/catnip/src/collections/async_slab.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 // Leaving this TODO for now, since I'm just going to fork `unicycle` for now.
 // Here's two main ideas for how we could do this efficiently.
 //
@@ -30,34 +31,43 @@
 //
 
 // use super::pin_slab::{PinSlab, SlabKey};
-// use uniset::BitSet;
+use uniset::BitSet;
 // use parking_lot::Mutex;
 // use std::sync::Arc;
 // use std::task::{Context, Wake, Waker, Poll, RawWaker, RawWakerVTable};
-// use std::pin::Pin;
+use std::pin::Pin;
 // use std::future::Future;
 // use futures::Stream;
 
-use bit_vec::BitVec;
+// use bit_vec::BitVec;
 use std::slice;
 use std::ops::Deref;
 use std::ptr::{self, NonNull};
-use std::cell::Cell;
+use std::cell::{RefCell, Cell};
 use std::alloc::{AllocRef, Global, Layout};
 use std::mem::{self, MaybeUninit};
 
+struct AllocOwned<T> {
+    has_free: BitSet,
+    pages: Vec<PagePtr<T>>
+}
+
 struct Root<T> {
     refcount: Cell<usize>,
-    ready: BitVec,
-    pages: Vec<PagePtr<T>>
+    shared_ready: RefCell<BitSet>,
+    owned: RefCell<AllocOwned<T>>,
 }
 
 impl<T> Root<T> {
     fn new() -> RootPtr<T> {
+        let owned = AllocOwned {
+            has_free: BitSet::new(),
+            pages: Vec::new(),
+        };
         let root = Self {
             refcount: Cell::new(1),
-            ready: BitVec::new(),
-            pages: Vec::new(),
+            shared_ready: RefCell::new(BitSet::new()),
+            owned: RefCell::new(owned),
         };
         let ptr = Global.alloc(Layout::for_value(&root)).expect("Allocation failed").cast();
         unsafe { ptr::write(ptr.as_ptr(), root) };
@@ -68,6 +78,60 @@ impl<T> Root<T> {
 
 struct RootPtr<T> {
     ptr: NonNull<Root<T>>,
+}
+
+impl<T> RootPtr<T> {
+    fn add_page(&self) -> usize {
+        let page_ix = {
+            let mut owned = self.owned.borrow_mut();
+            let page_ptr = Page::new(self.clone());
+            let page_ix = owned.pages.len();
+            owned.pages.push(page_ptr);
+            owned.has_free.set(page_ix);
+            page_ix
+        };
+        self.shared_ready.borrow_mut().reserve(page_ix + 1);
+        page_ix
+    }
+
+    fn alloc(&self, value: T) -> (usize, usize) {
+        {
+            let mut owned = self.owned.borrow_mut();
+            for slab_ix in owned.has_free.iter() {
+                let (page_ix, newly_full) = owned.pages[slab_ix].alloc(value);
+                if newly_full {
+                    owned.has_free.clear(slab_ix);
+                }
+                return (slab_ix, page_ix);
+            }
+        }
+        let slab_ix = self.add_page();
+        {
+            let mut owned = self.owned.borrow_mut();
+            let (page_ix, newly_full) = owned.pages[slab_ix].alloc(value);
+            assert!(!newly_full);
+            (slab_ix, page_ix)
+        }
+    }
+
+    fn free(&self, slab_ix: usize, page_ix: usize) -> bool {
+        let mut owned = self.owned.borrow_mut();
+        if slab_ix >= owned.pages.len() {
+            return false;
+        }
+        if !owned.pages[slab_ix].free(page_ix) {
+            return false;
+        }
+        if !owned.has_free.test(slab_ix) {
+            owned.has_free.set(slab_ix);
+        }
+        true
+    }
+
+    fn get(&self, slab_ix: usize, page_ix: usize) -> Option<&T> {
+        let mut owned = self.owned.borrow();
+        Some(unsafe { &*owned.pages.get(slab_ix)?.get(page_ix)? })
+    }
 }
 
 impl<T> Clone for RootPtr<T> {
@@ -108,72 +172,9 @@ impl<T> Drop for RootPtr<T> {
     }
 }
 
-struct PageHeader<T> {
-    refcount: Cell<usize>,
-    root: RootPtr<T>,
-    allocated: u64,
-    ready: u64,
-}
-
-struct Page<T> {
-    header: PageHeader<T>,
-    // Actually an array of values (see Self::layout()).
-    values: MaybeUninit<T>,
-}
 
 struct PagePtr<T> {
     ptr: NonNull<Page<T>>,
-}
-
-impl<T> Page<T> {
-    const fn layout() -> (Layout, usize) {
-        let page_size = 4096;
-        let header_layout = Layout::new::<PageHeader<T>>();
-
-        assert!(header_layout.align() <= page_size);
-
-        // No ZSTs here!
-        let value_layout = Layout::new::<T>();
-        assert!(value_layout.size() > 0);
-
-        let padding = header_layout.padding_needed_for(value_layout.align());
-
-        // Check that we have at least space for one value.
-        assert!(header_layout.size() + padding + value_layout.size() <= page_size);
-
-        let num_values = (page_size - (header_layout.size() + padding)) / value_layout.size();
-
-        // TODO: This alignment is too strict. For `n` values, we only need the pointer to be of
-        // alignment `n`: For element `i` we can hand out pointer `ptr as *mut u8 + i`.
-        (unsafe { Layout::from_size_align_unchecked(page_size, page_size) }, num_values)
-    }
-
-    pub fn new(root: RootPtr<T>) -> PagePtr<T> {
-        let page = Self {
-            header: PageHeader {
-                refcount: Cell::new(1),
-                root,
-                allocated: 0,
-                ready: 0,
-            },
-            values: MaybeUninit::uninit(),
-        };
-        let (layout, num_values) = Self::layout();
-        let ptr = Global.alloc(layout).expect("Allocation failed").cast();
-        unsafe { ptr::write(ptr.as_ptr(), page) };
-
-        let values: &mut [MaybeUninit<T>] = unsafe {
-            slice::from_raw_parts_mut(&mut (*ptr.as_ptr()).values as *mut _, num_values)
-        };
-        for v in values.iter_mut() {
-            *v = MaybeUninit::zeroed();
-        }
-        PagePtr { ptr }
-    }
-
-    pub fn alloc(&self, value: T) -> Result<usize, T> {
-        todo!();
-    }
 }
 
 impl<T> Clone for PagePtr<T> {
@@ -205,13 +206,145 @@ impl<T> Drop for PagePtr<T> {
         }
         if refcount == 1 {
             unsafe {
-                Global.dealloc(self.ptr.cast(), Layout::for_value(self.ptr.as_ref()));
+                ptr::drop_in_place(self.ptr.as_ptr());
+                let (layout, _) = Page::<T>::layout();
+                Global.dealloc(self.ptr.cast(), layout);
             }
         } else {
             root.header.refcount.set(refcount - 1);
         }
     }
 }
+
+struct PageHeader<T> {
+    refcount: Cell<usize>,
+    root: RootPtr<T>,
+    allocated: Cell<u64>,
+    ready: Cell<u64>,
+}
+
+struct Page<T> {
+    header: PageHeader<T>,
+    // Actually an array of values (see Self::layout()).
+    values: MaybeUninit<T>,
+}
+
+impl<T> Page<T> {
+    const fn layout() -> (Layout, usize) {
+        let page_size = 4096;
+        let header_layout = Layout::new::<PageHeader<T>>();
+
+        assert!(header_layout.align() <= page_size);
+
+        // No ZSTs here!
+        let value_layout = Layout::new::<T>();
+        assert!(value_layout.size() > 0);
+
+        let padding = header_layout.padding_needed_for(value_layout.align());
+
+        // Check that we have at least space for one value.
+        assert!(header_layout.size() + padding + value_layout.size() <= page_size);
+
+        let num_values = (page_size - (header_layout.size() + padding)) / value_layout.size();
+
+        // TODO: This alignment is too strict. For `n` values, we only need the pointer to be of
+        // alignment `n`: For element `i` we can hand out pointer `ptr as *mut u8 + i`.
+        (unsafe { Layout::from_size_align_unchecked(page_size, page_size) }, num_values)
+    }
+
+    pub fn new(root: RootPtr<T>) -> PagePtr<T> {
+        let page = Self {
+            header: PageHeader {
+                refcount: Cell::new(1),
+                root,
+                allocated: Cell::new(0),
+                ready: Cell::new(0),
+            },
+            values: MaybeUninit::uninit(),
+        };
+        let (layout, num_values) = Self::layout();
+        let ptr = Global.alloc(layout).expect("Allocation failed").cast();
+        unsafe { ptr::write(ptr.as_ptr(), page) };
+
+        let values: &mut [MaybeUninit<T>] = unsafe {
+            slice::from_raw_parts_mut(&mut (*ptr.as_ptr()).values as *mut _, num_values)
+        };
+        for v in values.iter_mut() {
+            *v = MaybeUninit::zeroed();
+        }
+        PagePtr { ptr }
+    }
+
+    unsafe fn values(&self) -> &mut [MaybeUninit<T>] {
+        let (_, num_values) = Self::layout();
+        slice::from_raw_parts_mut(&self.values as *const _ as *mut _, num_values)
+    }
+
+    pub fn alloc(&self, value: T) -> (usize, bool) {
+        let allocated = self.header.allocated.get();
+        let first_free = allocated.trailing_ones();
+        if first_free == 64 {
+            panic!("Allocated in full page");
+        }
+        let ix = first_free as usize;
+
+        debug_assert_eq!(allocated & (1 << first_free), 0);
+        let new_allocated = allocated | (1 << first_free);
+        self.header.allocated.set(new_allocated);
+        unsafe {
+            self.values()[ix].write(value);
+        }
+        let newly_full = new_allocated == u64::MAX;
+        (ix, newly_full)
+    }
+
+    pub fn free(&self, ix: usize) -> bool {
+        let (_, num_values) = Self::layout();
+        assert!(ix < num_values);
+
+        let allocated = self.header.allocated.get();
+        if allocated & (1 << ix) == 0 {
+            return false;
+        }
+        unsafe {
+            ptr::drop_in_place(&mut self.values()[ix] as *mut _);
+        };
+        self.header.allocated.set(allocated & !(1 << ix));
+        true
+    }
+
+    pub fn get(&self, ix: usize) -> Option<*mut T> {
+        let (_, num_values) = Self::layout();
+        if ix >= num_values {
+            return None;
+        }
+        let allocated = self.header.allocated.get();
+        if allocated & (1 << ix) == 0 {
+            return None;
+        }
+        unsafe {
+            Some(self.values()[ix].as_mut_ptr())
+        }
+    }
+}
+
+impl<T> Drop for Page<T> {
+    fn drop(&mut self) {
+        // Adapted from https://lemire.me/blog/2018/02/21/iterating-over-set-bits-quickly/
+        let mut allocated = self.header.allocated.get();
+        let values = unsafe { self.values() };
+
+        while allocated != 0 {
+            let t = allocated & allocated.wrapping_neg();
+            let ix = allocated.trailing_zeros();
+            unsafe {
+                ptr::drop_in_place(&mut values[ix as usize] as *mut _);
+            }
+            allocated ^= t;
+        }
+    }
+}
+
 
 
 // const PAGE_SIZE: usize = 32;
