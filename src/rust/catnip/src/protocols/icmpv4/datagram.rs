d9c031b99cc1b5a724e97f0bc305c36589f4b415
Sujay Jayakar <sujayakar314@gmail.com>
2020-11-05 22:26:09 +0100

    Add body to checksum for ICMPv4

--- src/rust/catnip/src/protocols/icmpv4/datagram.rs
+++ src/rust/catnip/src/protocols/icmpv4/datagram.rs
@@ -157,7 +157,7 @@ impl Icmpv4Header {
         let type_byte = hdr_buf[0];
         let code = hdr_buf[1];
         let checksum = NetworkEndian::read_u16(&hdr_buf[2..4]);
-        if checksum != icmpv4_checksum(hdr_buf) {
+        if checksum != icmpv4_checksum(hdr_buf, &data_buf) {
             return Err(Fail::Malformed {
                 details: "ICMPv4 checksum mismatch",
             });
@@ -173,22 +173,29 @@ impl Icmpv4Header {
         let (type_byte, rest_of_header) = self.icmpv4_type.serialize();
         buf[0] = type_byte;
         buf[1] = self.code;
-
         // Skip the checksum for now.
-
         buf[4..8].copy_from_slice(&rest_of_header[..]);
-        let checksum = icmpv4_checksum(buf);
+        let checksum = icmpv4_checksum(buf, &[]);
         NetworkEndian::write_u16(&mut buf[2..4], checksum);
     }
 }
 
-fn icmpv4_checksum(buf: &[u8; ICMPV4_HEADER2_SIZE]) -> u16 {
+fn icmpv4_checksum(buf: &[u8; ICMPV4_HEADER2_SIZE], body: &[u8]) -> u16 {
     let mut state = 0xffffu32;
     state += NetworkEndian::read_u16(&buf[0..2]) as u32;
     // Skip the checksum.
     state += 0;
     state += NetworkEndian::read_u16(&buf[4..6]) as u32;
     state += NetworkEndian::read_u16(&buf[6..8]) as u32;
+
+    let mut chunks_iter = body.chunks_exact(2);
+    while let Some(chunk) = chunks_iter.next() {
+        state += NetworkEndian::read_u16(chunk) as u32;
+    }
+    if let Some(&b) = chunks_iter.remainder().get(0) {
+        state += NetworkEndian::read_u16(&[b, 0]) as u32;
+    }
+
     while state > 0xFFFF {
         state -= 0xFFFF;
     }
