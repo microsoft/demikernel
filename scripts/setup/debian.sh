a7a5eeddd5ce6b5474d4fa85ba45368efd0f76c1
Pedro Henrique Penna <ppenna@microsoft.com>
2021-07-12 06:45:05 -0700

    [scripts] Enhancement: Updating Setup Scripts

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -5,13 +5,9 @@
 
 set -e
 
-APT_PACKAGES="cmake-curses-gui cmake-qt-gui build-essential clang libnuma-dev libmnl-dev libelf-dev libboost-dev libboost-program-options-dev libboost-coroutine-dev libboost-system-dev libboost-chrono-dev libyaml-cpp-dev libpcap-dev pkg-config python3 python3-pip"
-
-repo_root=$(git rev-parse --show-toplevel)
+APT_PACKAGES="build-essential clang libnuma-dev pkg-config python3 python3-pip"
 
 apt-get update
 apt-get -y install $APT_PACKAGES
 
 pip3 install pyelftools ninja meson
-
-$SHELL ${repo_root}/submodules/spdk/scripts/pkgdep.sh
--- scripts/setup/dpdk.sh
+++ scripts/setup/dpdk.sh
@@ -3,21 +3,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-# to be run every time the machine is rebooted.
-
-# sudo PCI_WHITELIST="0000:12:00.0" ~/proj/demikernel/submodules/spdk/scripts/setup.sh config
-echo 1024 | sudo tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages
-mkdir /mnt/huge || true
-mount -t hugetlbfs nodev /mnt/huge
 modprobe ib_uverbs
 modprobe mlx4_ib
-#cpupower frequency-set --governor performance
-#sysctl -p
-#ethtool -K ens1 lro on
-#ifconfig ens1 txqueuelen 20000
-#systemctl stop irqbalance
-#export MLX4_SINGLE_THREADED=1
-#export MLX5_SINGLE_THREADED=1
-export MLX5_SHUT_UP_BF=0
-export MLX_QP_ALLOC_TYPE="HUGE"
-export MLX_CQ_ALLOC_TYPE="HUGE"
