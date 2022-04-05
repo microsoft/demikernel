ddac7f2ee1200cf39d9eddd02365d0e8680476ed
Pedro Henrique Penna <ppenna@microsoft.com>
2022-04-05 10:14:04 -0300

    [test] Enhancement: Drop Unused TryBlocks Feature

--- tests/pingpong.rs
+++ tests/pingpong.rs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-#![feature(try_blocks)]
-
 mod common;
 
 //==============================================================================
