1fb576b4e19b4f1864b1df4b610aceec74cd884f
Pedro Henrique Penna <ppenna@microsoft.com>
2022-04-18 09:05:25 -0300

    [catnap] Enhancement: Cleanup Polling from Traces

--- src/catnap/futures/pushto.rs
+++ src/catnap/futures/pushto.rs
@@ -80,7 +80,6 @@ impl Future for PushtoFuture {
             },
             // Operation in progress.
             Err(e) if e == Errno::EWOULDBLOCK || e == Errno::EAGAIN => {
-                trace!("push in progress ({:?})", e);
                 ctx.waker().wake_by_ref();
                 Poll::Pending
             },
