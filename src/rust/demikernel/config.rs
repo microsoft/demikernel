40b6d66663e5271df691276443db4c92ee9272b8
Pedro Henrique Penna <ppenna@microsoft.com>
2022-08-04 11:59:00 -0300

    [pdpix] Enhancement: Adding Type Annotations

--- src/rust/demikernel/config.rs
+++ src/rust/demikernel/config.rs
@@ -1,10 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-use ::anyhow::{
-    format_err,
-    Error,
-};
+use ::anyhow::Error;
 use ::runtime::network::types::MacAddress;
 use ::std::{
     collections::HashMap,
@@ -39,17 +36,17 @@ pub struct Config {
 
 impl Config {
     pub fn arp_table(&self) -> HashMap<Ipv4Addr, MacAddress> {
-        let mut arp_table = HashMap::new();
+        let mut arp_table: HashMap<Ipv4Addr, MacAddress> = HashMap::new();
         if let Some(arp_table_obj) = self.config_obj["catnip"]["arp_table"].as_hash() {
             for (k, v) in arp_table_obj {
-                let link_addr_str = k
+                let link_addr_str: &str = k
                     .as_str()
-                    .ok_or_else(|| format_err!("Couldn't find ARP table link_addr in config"))
+                    .ok_or_else(|| anyhow::format_err!("Couldn't find ARP table link_addr in config"))
                     .unwrap();
                 let link_addr = MacAddress::parse_str(link_addr_str).unwrap();
                 let ipv4_addr: Ipv4Addr = v
                     .as_str()
-                    .ok_or_else(|| format_err!("Couldn't find ARP table link_addr in config"))
+                    .ok_or_else(|| anyhow::format_err!("Couldn't find ARP table link_addr in config"))
                     .unwrap()
                     .parse()
                     .unwrap();
@@ -66,7 +63,7 @@ impl Config {
                 .iter()
                 .map(|a| {
                     a.as_str()
-                        .ok_or_else(|| format_err!("Non string argument"))
+                        .ok_or_else(|| anyhow::format_err!("Non string argument"))
                         .and_then(|s| CString::new(s).map_err(|e| e.into()))
                 })
                 .collect::<Result<Vec<_>, Error>>()
@@ -76,18 +73,18 @@ impl Config {
     }
 
     pub fn new(config_path: String) -> Self {
-        let mut config_s = String::new();
+        let mut config_s: String = String::new();
         File::open(config_path).unwrap().read_to_string(&mut config_s).unwrap();
-        let config = YamlLoader::load_from_str(&config_s).unwrap();
-        let config_obj = match &config[..] {
+        let config: Vec<Yaml> = YamlLoader::load_from_str(&config_s).unwrap();
+        let config_obj: &Yaml = match &config[..] {
             &[ref c] => c,
-            _ => Err(format_err!("Wrong number of config objects")).unwrap(),
+            _ => Err(anyhow::format_err!("Wrong number of config objects")).unwrap(),
         };
 
         // Parse local IPv4 address.
         let local_ipv4_addr: Ipv4Addr = config_obj["catnip"]["my_ipv4_addr"]
             .as_str()
-            .ok_or_else(|| format_err!("Couldn't find my_ipv4_addr in config"))
+            .ok_or_else(|| anyhow::format_err!("Couldn't find my_ipv4_addr in config"))
             .unwrap()
             .parse()
             .unwrap();
@@ -99,14 +96,14 @@ impl Config {
         let local_link_addr: MacAddress = MacAddress::parse_str(
             config_obj["catnip"]["my_link_addr"]
                 .as_str()
-                .ok_or_else(|| format_err!("Couldn't find my_link_addr in config"))
+                .ok_or_else(|| anyhow::format_err!("Couldn't find my_link_addr in config"))
                 .unwrap(),
         )
         .unwrap();
 
         let local_interface_name = config_obj["catnip"]["my_interface_name"]
             .as_str()
-            .ok_or_else(|| format_err!("Couldn't find my_interface_name config"))
+            .ok_or_else(|| anyhow::format_err!("Couldn't find my_interface_name config"))
             .unwrap();
 
         // Parse ARP table.
@@ -115,11 +112,11 @@ impl Config {
             disable_arp = arp_disabled;
         }
         // Parse network parameters.
-        let use_jumbo_frames = env::var("USE_JUMBO").is_ok();
+        let use_jumbo_frames: bool = env::var("USE_JUMBO").is_ok();
         let mtu: u16 = env::var("MTU").unwrap().parse().unwrap();
         let mss: usize = env::var("MSS").unwrap().parse().unwrap();
-        let udp_checksum_offload = env::var("UDP_CHECKSUM_OFFLOAD").is_ok();
-        let tcp_checksum_offload = env::var("TCP_CHECKSUM_OFFLOAD").is_ok();
+        let udp_checksum_offload: bool = env::var("UDP_CHECKSUM_OFFLOAD").is_ok();
+        let tcp_checksum_offload: bool = env::var("TCP_CHECKSUM_OFFLOAD").is_ok();
 
         Self {
             use_jumbo_frames,
