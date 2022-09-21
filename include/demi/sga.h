c89a444e9fb14d2171fdb238001717362d631134
Pedro Henrique Penna <ppenna@microsoft.com>
2022-09-21 11:31:23 -0300

    [doc] Enhancement: Improving API Docs

--- include/demi/sga.h
+++ include/demi/sga.h
@@ -12,8 +12,24 @@ extern "C"
 {
 #endif
 
+    /**
+     * @brief Allocates a scatter-gather array.
+     *
+     * @param size Size of the scatter-gather array.
+     *
+     * @return On successful completion, the allocated scatter-gather array is returned. On error, a null scatter-gather
+     * array is returned instead.
+     */
+    extern demi_sgarray_t demi_sgaalloc(size_t size);
+
+    /**
+     * @brief Releases a scatter-gather array.
+     *
+     * @param sga Target scatter-gather array.
+     *
+     * @return On successful completion, zero is returned. On failure, a positive error code is returned instead.
+     */
     extern int demi_sgafree(demi_sgarray_t *sga);
-    extern demi_sgarray_t demi_sgaalloc(size_t len);
 
 #ifdef __cplusplus
 }
--- include/demi/types.h
+++ include/demi/types.h
@@ -14,49 +14,73 @@ extern "C"
 {
 #endif
 
+/**
+ * @brief Maximum number of segments in a scatter-gather array.
+ */
 #define DEMI_SGARRAY_MAXSIZE 1
 
+    /**
+     * @brief An I/O queue token.
+     */
     typedef uint64_t demi_qtoken_t;
 
+    /**
+     * @brief A segment of a scatter-gather array.
+     */
     typedef struct demi_sgaseg
     {
-        void *sgaseg_buf;
-        uint32_t sgaseg_len;
+        void *sgaseg_buf;    /**< Underlying data.       */
+        uint32_t sgaseg_len; /**< Size in bytes of data. */
     } demi_sgaseg_t;
 
+    /**
+     * @brief A scatter-gather array.
+     */
     typedef struct demi_sgarray
     {
-        void *sga_buf;
-        uint32_t sga_numsegs;
-        demi_sgaseg_t sga_segs[DEMI_SGARRAY_MAXSIZE];
-        // TODO: Drop the following field.
-        struct sockaddr_in sga_addr;
+        void *sga_buf;                                /**< Reserved.                                       */
+        uint32_t sga_numsegs;                         /**< Number of segments in the scatter-gather array. */
+        demi_sgaseg_t sga_segs[DEMI_SGARRAY_MAXSIZE]; /**< Scatter-gather array segments.                  */
+        struct sockaddr_in sga_addr;                  /**< Source address of scatter-gather array.         */
     } demi_sgarray_t;
 
+    /**
+     * @brief Opcodes for an asynchronous I/O operation.
+     */
     typedef enum demi_opcode
     {
-        DEMI_OPC_INVALID = 0,
-        DEMI_OPC_PUSH,
-        DEMI_OPC_POP,
-        DEMI_OPC_ACCEPT,
-        DEMI_OPC_CONNECT,
-        DEMI_OPC_FAILED,
+        DEMI_OPC_INVALID = 0, /**< Invalid operation. */
+        DEMI_OPC_PUSH,        /**< Push operation.    */
+        DEMI_OPC_POP,         /**< Pop operation.     */
+        DEMI_OPC_ACCEPT,      /**< Accept operation.  */
+        DEMI_OPC_CONNECT,     /**< Connect operation. */
+        DEMI_OPC_FAILED,      /**< Operation failed.  */
     } demi_opcode_t;
 
+    /**
+     * @brief Result value for an accept operation.
+     */
     typedef struct demi_accept_result
     {
-        int qd;
-        struct sockaddr_in addr;
+        int qd;                  /**< Socket I/O queue descriptor of accepted connection. */
+        struct sockaddr_in addr; /**< Remote address of accepted connection.              */
     } demi_accept_result_t;
 
+    /**
+     * @brief Result value for an asynchronous I/O operation.
+     */
     typedef struct demi_qresult
     {
-        enum demi_opcode qr_opcode;
-        int qr_qd;
-        demi_qtoken_t qr_qt;
+        enum demi_opcode qr_opcode; /**< Opcode of completed operation.                              */
+        int qr_qd;                  /**< I/O queue descriptor associated to the completed operation. */
+        demi_qtoken_t qr_qt;        /**< I/O queue token of the completed operation.                 */
+
+        /**
+         * @brief Result value.
+         */
         union {
-            demi_sgarray_t sga;
-            demi_accept_result_t ares;
+            demi_sgarray_t sga;        /**< Pushed/popped scatter-gather array. */
+            demi_accept_result_t ares; /**< Accept result.                      */
         } qr_value;
     } demi_qresult_t;
 
