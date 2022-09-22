8ef08427b9139817deb83907fc8c1876b4bd34ff
Pedro Henrique Penna <ppenna@microsoft.com>
2022-09-22 08:09:14 -0300

    [examples] Feature: UDP Dump

---
+++ examples/rust/udp-dump.rs
@@ -0,0 +1,178 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#![cfg_attr(feature = "strict", deny(warnings))]
+#![deny(clippy::all)]
+
+//==============================================================================
+// Imports
+//==============================================================================
+
+use ::anyhow::Result;
+use ::clap::{
+    Arg,
+    ArgMatches,
+    Command,
+};
+use ::demikernel::{
+    LibOS,
+    LibOSName,
+    OperationResult,
+    QDesc,
+    QToken,
+};
+use ::std::{
+    net::SocketAddrV4,
+    str::FromStr,
+    time::{
+        Duration,
+        Instant,
+    },
+};
+
+//==============================================================================
+// Program Arguments
+//==============================================================================
+
+/// Program Arguments
+#[derive(Debug)]
+struct ProgramArguments {
+    /// Local socket IPv4 address.
+    local: SocketAddrV4,
+}
+
+/// Associate functions for Program Arguments
+impl ProgramArguments {
+    /// Default local address.
+    const DEFAULT_LOCAL: &'static str = "127.0.0.1:12345";
+
+    /// Parses the program arguments from the command line interface.
+    pub fn new(app_name: &str, app_author: &str, app_about: &str) -> Result<Self> {
+        let matches: ArgMatches = Command::new(app_name)
+            .author(app_author)
+            .about(app_about)
+            .arg(
+                Arg::new("local")
+                    .long("local")
+                    .takes_value(true)
+                    .required(false)
+                    .value_name("ADDRESS:PORT")
+                    .help("Sets local address"),
+            )
+            .get_matches();
+
+        // Default arguments.
+        let mut args: ProgramArguments = ProgramArguments {
+            local: SocketAddrV4::from_str(Self::DEFAULT_LOCAL)?,
+        };
+
+        // Local address.
+        if let Some(addr) = matches.value_of("local") {
+            args.set_local_addr(addr)?;
+        }
+
+        Ok(args)
+    }
+
+    /// Returns the local endpoint address parameter stored in the target program arguments.
+    pub fn get_local(&self) -> SocketAddrV4 {
+        self.local
+    }
+
+    /// Sets the local address and port number parameters in the target program arguments.
+    fn set_local_addr(&mut self, addr: &str) -> Result<()> {
+        self.local = SocketAddrV4::from_str(addr)?;
+        Ok(())
+    }
+}
+
+//==============================================================================
+// Application
+//==============================================================================
+
+/// Application
+struct Application {
+    /// Underlying libOS.
+    libos: LibOS,
+    /// Local socket descriptor.
+    sockqd: QDesc,
+}
+
+/// Associated Functions for the Application
+impl Application {
+    /// Logging interval (in seconds).
+    const LOG_INTERVAL: u64 = 5;
+
+    /// Instantiates the application.
+    pub fn new(mut libos: LibOS, args: &ProgramArguments) -> Self {
+        // Extract arguments.
+        let local: SocketAddrV4 = args.get_local();
+
+        // Create UDP socket.
+        let sockqd: QDesc = match libos.socket(libc::AF_INET, libc::SOCK_DGRAM, 0) {
+            Ok(qd) => qd,
+            Err(e) => panic!("failed to create socket: {:?}", e.cause),
+        };
+
+        // Bind to local address.
+        match libos.bind(sockqd, local) {
+            Ok(()) => (),
+            Err(e) => panic!("failed to bind socket: {:?}", e.cause),
+        };
+
+        println!("Local Address: {:?}", local);
+
+        Self { libos, sockqd }
+    }
+
+    /// Runs the target application.
+    pub fn run(&mut self) -> ! {
+        let start: Instant = Instant::now();
+        let mut nbytes: usize = 0;
+        let mut last_log: Instant = Instant::now();
+
+        loop {
+            // Dump statistics.
+            if last_log.elapsed() > Duration::from_secs(Self::LOG_INTERVAL) {
+                let elapsed: Duration = Instant::now() - start;
+                println!("{:?} B / {:?} us", nbytes, elapsed.as_micros());
+                last_log = Instant::now();
+            }
+
+            // Drain packets.
+            let qt: QToken = match self.libos.pop(self.sockqd) {
+                Ok(qt) => qt,
+                Err(e) => panic!("failed to pop data from socket: {:?}", e.cause),
+            };
+            match self.libos.wait2(qt) {
+                Ok((_, OperationResult::Pop(_, buf))) => {
+                    nbytes += buf.len();
+                },
+                Err(e) => panic!("operation failed: {:?}", e.cause),
+                _ => panic!("unexpected result"),
+            }
+        }
+    }
+}
+
+//==============================================================================
+
+/// Drives the application.
+fn main() -> Result<()> {
+    let args: ProgramArguments = ProgramArguments::new(
+        "udp-dump",
+        "Pedro Henrique Penna <ppenna@microsoft.com>",
+        "Dumps incoming packets on a UDP port.",
+    )?;
+
+    let libos_name: LibOSName = match LibOSName::from_env() {
+        Ok(libos_name) => libos_name.into(),
+        Err(e) => panic!("{:?}", e),
+    };
+    let libos: LibOS = match LibOS::new(libos_name) {
+        Ok(libos) => libos,
+        Err(e) => panic!("failed to initialize libos: {:?}", e.cause),
+    };
+
+    Application::new(libos, &args).run();
+}
