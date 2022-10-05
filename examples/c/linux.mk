c61ec00fa88a733ee69e63fac343574e8539777b
Pedro Henrique Penna <ppenna@microsoft.com>
2022-10-05 11:30:35 -0300

    [examples] Feature: Pipe Ping Pong

--- examples/c/linux.mk
+++ examples/c/linux.mk
@@ -28,10 +28,14 @@ export COMPILE_CMD = $(CC) $(CFLAGS) $@.o common.o -o $(BINDIR)/examples/c/$@.$(
 #=======================================================================================================================
 
 # Builds everything.
-all: common.o udp-push-pop udp-ping-pong tcp-push-pop tcp-ping-pong
+all: common.o pipe-ping-pong udp-push-pop udp-ping-pong tcp-push-pop tcp-ping-pong
 
 make-dirs:
 	mkdir -p $(BINDIR)/examples/c
+
+# Builds pipe ping pong test.
+pipe-ping-pong: make-dirs pipe-ping-pong.o
+	$(COMPILE_CMD)
 
 # Builds UDP push pop test.
 udp-push-pop: make-dirs common.o udp-push-pop.o
@@ -52,6 +56,7 @@ tcp-ping-pong: make-dirs common.o tcp-ping-pong.o
 # Cleans up all build artifacts.
 clean:
 	@rm -rf $(OBJ)
+	@rm -rf $(BINDIR)/examples/c/pipe-ping-pong.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/c/udp-push-pop.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/c/udp-ping-pong.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/c/tcp-push-pop.$(EXEC_SUFFIX)
