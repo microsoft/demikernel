17db46d3285432d7e857eef40df4807f17a0ef69
Pedro Henrique Penna <ppenna@microsoft.com>
2022-10-05 11:29:33 -0300

    [examples] Feature: Pipe Push Pop

---
+++ examples/c/pipe-push-pop.c
@@ -0,0 +1,252 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// This should come first.
+// Glibc macro to expose definitions corresponding to the POSIX.1-2008 base specification.
+// See https://man7.org/linux/man-pages/man7/feature_test_macros.7.html.
+#define _POSIX_C_SOURCE 200809L
+
+/*====================================================================================================================*
+ * Imports                                                                                                            *
+ *====================================================================================================================*/
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <demi/libos.h>
+#include <demi/sga.h>
+#include <demi/wait.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+/*====================================================================================================================*
+ * Constants                                                                                                          *
+ *====================================================================================================================*/
+
+/**
+ * @brief Data size.
+ */
+#define DATA_SIZE 64
+
+/**
+ * @brief Maximum number of bytes to transfer.
+ */
+#define MAX_BYTES (DATA_SIZE * 1024)
+
+/*====================================================================================================================*
+ * sighandler()                                                                                                       *
+ *====================================================================================================================*/
+
+/**
+ * @brief Signal handler.
+ *
+ * @param signum Number of received signal.
+ */
+static void sighandler(int signum)
+{
+    const char *signame = strsignal(signum);
+
+    fprintf(stderr, "\nReceived %s signal\n", signame);
+    fprintf(stderr, "Exiting...\n");
+
+    exit(EXIT_SUCCESS);
+}
+
+/*====================================================================================================================*
+ * push_wait()                                                                                                        *
+ *====================================================================================================================*/
+
+/**
+ * @brief Pushes a scatter-gather array to a remote socket and waits for operation to complete.
+ *
+ * @param qd  Target queue descriptor.
+ * @param sga Target scatter-gather array.
+ * @param qr  Storage location for operation result.
+ */
+static void push_wait(int qd, demi_sgarray_t *sga, demi_qresult_t *qr)
+{
+    demi_qtoken_t qt = -1;
+
+    /* Push data. */
+    assert(demi_push(&qt, qd, sga) == 0);
+
+    /* Wait push operation to complete. */
+    assert(demi_wait(qr, qt, NULL) == 0);
+
+    /* Parse operation result. */
+    assert(qr->qr_opcode == DEMI_OPC_PUSH);
+}
+
+/*====================================================================================================================*
+ * pop_wait()                                                                                                         *
+ *====================================================================================================================*/
+
+/**
+ * @brief Pops a scatter-gather array and waits for operation to complete.
+ *
+ * @param qd Target queue descriptor.
+ * @param qr Storage location for operation result.
+ */
+static void pop_wait(int qd, demi_qresult_t *qr)
+{
+    demi_qtoken_t qt = -1;
+
+    /* Pop data. */
+    assert(demi_pop(&qt, qd) == 0);
+
+    /* Wait for pop operation to complete. */
+    assert(demi_wait(qr, qt, NULL) == 0);
+
+    /* Parse operation result. */
+    assert(qr->qr_opcode == DEMI_OPC_POP);
+    assert(qr->qr_value.sga.sga_segs != 0);
+}
+
+/*====================================================================================================================*
+ * server()                                                                                                           *
+ *====================================================================================================================*/
+
+/**
+ * @brief Memory queue echo server.
+ *
+ * @param argc Argument count.
+ * @param argv Argument list.
+ */
+static void server(int argc, char *const argv[])
+{
+    int nbytes = 0;
+    char name[1024];
+    int pipeqd_rx = -1;
+
+    /* Initialize demikernel */
+    assert(demi_init(argc, argv) == 0);
+
+    /* Setup memory queues. */
+    sprintf(name, "%s:rx", argv[2]);
+    assert(demi_create_pipe(&pipeqd_rx, name) == 0);
+
+    /* Run. */
+    while (nbytes < MAX_BYTES)
+    {
+        demi_qresult_t qr = {0};
+        demi_sgarray_t sga = {0};
+
+        /* Pop scatter-gather array. */
+        pop_wait(pipeqd_rx, &qr);
+
+        /* Extract received scatter-gather array. */
+        memcpy(&sga, &qr.qr_value.sga, sizeof(demi_sgarray_t));
+
+        nbytes += sga.sga_segs[0].sgaseg_len;
+
+        /* Release received scatter-gather array. */
+        assert(demi_sgafree(&sga) == 0);
+
+        fprintf(stdout, "pop (%d)\n", nbytes);
+    }
+}
+
+/*====================================================================================================================*
+ * client()                                                                                                           *
+ *====================================================================================================================*/
+
+/**
+ * @brief Memory queue echo client.
+ *
+ * @param argc Argument count.
+ * @param argv Argument list.
+ */
+static void client(int argc, char *const argv[])
+{
+    int nbytes = 0;
+    char name[1024];
+    int pipeqd_tx = -1;
+
+    /* Initialize demikernel */
+    assert(demi_init(argc, argv) == 0);
+
+    /* Setup memory queues. */
+    sprintf(name, "%s:rx", argv[2]);
+    assert(demi_open_pipe(&pipeqd_tx, name) == 0);
+
+    /* Run. */
+    while (nbytes < MAX_BYTES)
+    {
+        demi_qresult_t qr = {0};
+        demi_sgarray_t sga = {0};
+
+        /* Allocate scatter-gather array. */
+        sga = demi_sgaalloc(DATA_SIZE);
+        assert(sga.sga_segs != 0);
+
+        /* Cook data. */
+        memset(sga.sga_segs[0].sgaseg_buf, 1, DATA_SIZE);
+
+        /* Push scatter-gather array. */
+        push_wait(pipeqd_tx, &sga, &qr);
+
+        nbytes += sga.sga_segs[0].sgaseg_len;
+
+        /* Release sent scatter-gather array. */
+        assert(demi_sgafree(&sga) == 0);
+
+        fprintf(stdout, "push (%d)\n", nbytes);
+    }
+}
+
+/*====================================================================================================================*
+ * usage()                                                                                                            *
+ *====================================================================================================================*/
+
+/**
+ * @brief Prints program usage.
+ *
+ * @param progname Program name.
+ */
+static void usage(const char *progname)
+{
+    fprintf(stderr, "Usage: %s MODE pipe-name\n", progname);
+    fprintf(stderr, "MODE:\n");
+    fprintf(stderr, "  --client    Run in client mode.\n");
+    fprintf(stderr, "  --server    Run in server mode.\n");
+}
+
+/*====================================================================================================================*
+ * main()                                                                                                             *
+ *====================================================================================================================*/
+
+/**
+ * @brief Exercises a one-way direction communication through UDP.
+ *
+ * This system-level test instantiates two demikernel nodes: a client and a server. The client sends UDP packets to the
+ * server in a tight loop. The server process in a tight loop received UDP packets from the client.
+ *
+ * @param argc Argument count.
+ * @param argv Argument list.
+ *
+ * @return On successful completion EXIT_SUCCESS is returned.
+ */
+int main(int argc, char *const argv[])
+{
+    /* Install signal handlers. */
+    signal(SIGINT, sighandler);
+    signal(SIGQUIT, sighandler);
+    signal(SIGTSTP, sighandler);
+
+    if (argc >= 3)
+    {
+        /* Run. */
+        if (!strcmp(argv[1], "--server"))
+            server(argc, argv);
+        else if (!strcmp(argv[1], "--client"))
+            client(argc, argv);
+
+        return (EXIT_SUCCESS);
+    }
+
+    usage(argv[0]);
+
+    return (EXIT_SUCCESS);
+}
---
+++ examples/rust/pipe-push-pop.rs
@@ -0,0 +1,210 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//======================================================================================================================
+// Imports
+//======================================================================================================================
+
+use ::anyhow::{
+    bail,
+    Result,
+};
+use ::demikernel::{
+    demi_sgarray_t,
+    runtime::types::demi_opcode_t,
+    LibOS,
+    LibOSName,
+    QDesc,
+    QToken,
+};
+use ::std::{
+    env,
+    panic,
+};
+use core::slice;
+
+//======================================================================================================================
+// Constants
+//======================================================================================================================
+
+/// Size of a data buffer.
+const BUFFER_SIZE: usize = 64;
+
+/// Number of rounds to execute.
+/// This should fit in a byte.
+const NROUNDS: u8 = 128;
+
+//======================================================================================================================
+// mksga()
+//======================================================================================================================
+
+/// Makes a scatter-gather array.
+fn mksga(libos: &mut LibOS, size: usize, value: u8) -> demi_sgarray_t {
+    // Allocate scatter-gather array.
+    let sga: demi_sgarray_t = match libos.sgaalloc(size) {
+        Ok(sga) => sga,
+        Err(e) => panic!("failed to allocate scatter-gather array: {:?}", e),
+    };
+
+    // Ensure that scatter-gather array has the requested size.
+    assert_eq!(sga.sga_segs[0].sgaseg_len as usize, size);
+
+    // Fill in scatter-gather array.
+    let ptr: *mut u8 = sga.sga_segs[0].sgaseg_buf as *mut u8;
+    let len: usize = sga.sga_segs[0].sgaseg_len as usize;
+    let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ptr, len) };
+    slice.fill(value);
+
+    sga
+}
+
+//======================================================================================================================
+// push_and_wait()
+//======================================================================================================================
+
+/// Pushes a scatter-gather array and waits for the operation to complete.
+fn push_and_wait(libos: &mut LibOS, pipeqd: QDesc, length: usize, value: u8) -> Result<()> {
+    let sga: demi_sgarray_t = mksga(libos, length, value);
+
+    let qt: QToken = match libos.push(pipeqd, &sga) {
+        Ok(qt) => qt,
+        Err(e) => bail!("failed to push: {:?}", e.cause),
+    };
+
+    libos.wait(qt, None)?;
+
+    if libos.sgafree(sga).is_err() {
+        bail!("failed to release scatter-gather array");
+    };
+
+    Ok(())
+}
+
+//======================================================================================================================
+// server()
+//======================================================================================================================
+
+fn server(name: &str) -> Result<()> {
+    let libos_name: LibOSName = match LibOSName::from_env() {
+        Ok(libos_name) => libos_name.into(),
+        Err(e) => panic!("{:?}", e),
+    };
+    let mut libos: LibOS = match LibOS::new(libos_name) {
+        Ok(libos) => libos,
+        Err(e) => panic!("failed to initialize libos: {:?}", e.cause),
+    };
+
+    // Setup peer.
+    let pipeqd: QDesc = match libos.create_pipe(&name) {
+        Ok(qd) => qd,
+        Err(e) => panic!("failed to open memory queue: {:?}", e.cause),
+    };
+
+    // Receive bytes.
+    let mut round: u8 = 0;
+    let mut nbytes: usize = 0;
+    while nbytes < (NROUNDS as usize * BUFFER_SIZE) {
+        // Pop data.
+        let qt: QToken = match libos.pop(pipeqd) {
+            Ok(qt) => qt,
+            Err(e) => panic!("pop failed: {:?}", e.cause),
+        };
+        let sga: demi_sgarray_t = match libos.wait(qt, None) {
+            Ok(qr) => match qr.qr_opcode {
+                demi_opcode_t::DEMI_OPC_POP => unsafe { qr.qr_value.sga },
+                _ => panic!("unexpected operation result"),
+            },
+            _ => panic!("server failed to wait()"),
+        };
+
+        let recvbuf: &[u8] = unsafe {
+            slice::from_raw_parts(
+                sga.sga_segs[0].sgaseg_buf as *const u8,
+                sga.sga_segs[0].sgaseg_len as usize,
+            )
+        };
+
+        // Sanity received data.
+        for x in &recvbuf[..] {
+            assert_eq!(*x, round);
+            nbytes += 1;
+            if nbytes % BUFFER_SIZE == 0 {
+                round += 1;
+            }
+        }
+
+        if libos.sgafree(sga).is_err() {
+            bail!("failed to release scatter-gather array");
+        };
+
+        println!("pop {:?}", round - 1);
+    }
+
+    libos.close(pipeqd)?;
+
+    Ok(())
+}
+
+//======================================================================================================================
+// client()
+//======================================================================================================================
+
+fn client(name: &str) -> Result<()> {
+    let libos_name: LibOSName = match LibOSName::from_env() {
+        Ok(libos_name) => libos_name.into(),
+        Err(e) => panic!("{:?}", e),
+    };
+    let mut libos: LibOS = match LibOS::new(libos_name) {
+        Ok(libos) => libos,
+        Err(e) => panic!("failed to initialize libos: {:?}", e.cause),
+    };
+
+    // Setup peer.
+    let pipeqd: QDesc = match libos.open_pipe(&name) {
+        Ok(qd) => qd,
+        Err(e) => panic!("failed to open memory queue: {:?}", e.cause),
+    };
+
+    // Send and receive bytes in a locked step.
+    for i in 0..NROUNDS {
+        push_and_wait(&mut libos, pipeqd, BUFFER_SIZE, i)?;
+        println!("push {:?}", i);
+    }
+
+    libos.close(pipeqd)?;
+
+    Ok(())
+}
+
+//======================================================================================================================
+// usage()
+//======================================================================================================================
+
+/// Prints program usage and exits.
+fn usage(program_name: &String) {
+    println!("Usage: {} MODE pipe-name", program_name);
+    println!("Modes:\n");
+    println!("  --client    Run program in client mode.");
+    println!("  --server    Run program in server mode.");
+}
+
+//======================================================================================================================
+// main()
+//======================================================================================================================
+
+pub fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 3 {
+        if args[1] == "--server" {
+            let ret: Result<()> = server(&args[2]);
+            return ret;
+        } else if args[1] == "--client" {
+            let ret: Result<()> = client(&args[2]);
+            return ret;
+        }
+    }
+
+    usage(&args[0]);
+
+    Ok(())
+}
