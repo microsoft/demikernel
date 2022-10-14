d70f305e91ea4da9240175ae0d6666cbe72fc1d7
Anand Bonde <anand.bonde@gmail.com>
2022-10-14 12:14:38 -0600

    [pal] Bug Fix: Pass function args by ref

--- src/rust/pal/functions.rs
+++ src/rust/pal/functions.rs
@@ -8,7 +8,7 @@ use crate::pal::data_structures::SockAddrIn;
 //======================================================================================================================
 
 #[cfg(target_os = "windows")]
-pub fn get_addr_from_sock_addr_in(sock_addr_in: SockAddrIn) -> u32 {
+pub fn get_addr_from_sock_addr_in(sock_addr_in: &SockAddrIn) -> u32 {
     unsafe { sock_addr_in.sin_addr.S_un.S_addr }
 }
 
@@ -17,6 +17,6 @@ pub fn get_addr_from_sock_addr_in(sock_addr_in: SockAddrIn) -> u32 {
 //======================================================================================================================
 
 #[cfg(target_os = "linux")]
-pub fn get_addr_from_sock_addr_in(sock_addr_in: SockAddrIn) -> u32 {
+pub fn get_addr_from_sock_addr_in(sock_addr_in: &SockAddrIn) -> u32 {
     sock_addr_in.sin_addr.s_addr
 }
