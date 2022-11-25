c0fd76c7e6bf3a8051b5a6015ad03b9a66a40753
Pedro Henrique Penna <ppenna@microsoft.com>
2022-11-25 10:51:44 -0300

    [toosl] Feature: Demikernel CI Tool

---
+++ tools/demikernel-ci.py
@@ -0,0 +1,254 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import sys
+import argparse
+import string
+import subprocess
+import time
+from typing import List
+
+# ======================================================================================================================
+# Utilities
+# ======================================================================================================================
+
+
+def timing(f):
+    def wrap(*args, **kwargs):
+        time1 = time.time()
+        ret = f(*args, **kwargs)
+        time2 = time.time()
+        duration: float = (time2-time1)*1000.0
+        return (ret, duration)
+    return wrap
+
+
+def wait_jobs(name: string, jobs: List):
+    @timing
+    def wait_jobs2(name: string, jobs: List) -> List:
+        status: list[int] = []
+
+        for j in jobs:
+            stdout, stderr = j.communicate()
+            status.append((j.pid, j.returncode))
+            job_name: string = "{}-{}".format(name, str(j.pid))
+            with open(job_name + ".stdout", "w") as file:
+                file.write("{}".format(stdout))
+            with open(job_name + ".stderr", "w") as file:
+                file.write("{}".format(stderr))
+
+        # Cleanup list of jobs.
+        while len(jobs) > 0:
+            jobs.pop(0)
+
+        return status
+    return wait_jobs2(name, jobs)
+
+
+def wait_and_report(name: string, jobs: List, all_pass=True):
+    ret = wait_jobs(name, jobs)
+    status: List = ret[0]
+    duration: float = ret[1]
+    if all_pass:
+        passed: bool = True if status[0][1] == 0 and status[1][1] == 0 else False
+    else:
+        passed: bool = True if status[0][1] == 0 or status[1][1] == 0 else False
+    print("[{}] in {:9.2f} ms {}".format("PASSED" if passed else "FAILED", duration, name))
+
+    return passed
+
+# ======================================================================================================================
+# Remote Commands
+# ======================================================================================================================
+
+
+# Executes a checkout command in a remote host.
+def remote_checkout(host: string, repository: string, branch: string):
+    cmd = "cd {} && git pull origin && git checkout {}".format(repository, branch)
+    ssh_cmd = "ssh {} \"bash -l -c \'{}\'\"".format(host, cmd)
+    return subprocess.Popen(ssh_cmd, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+# Executes a compile command in a remote host.
+def remote_compile(host: string, repository: string, target: string, is_debug: bool):
+    debug_flag: string = "DEBUG=yes" if is_debug else "DEBUG=no"
+    cmd = "cd {} && make {} {}".format(repository, debug_flag, target)
+    ssh_cmd = "ssh {} \"bash -l -c \'{}\'\"".format(host, cmd)
+    return subprocess.Popen(ssh_cmd, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+# Executes a test in a remote host.
+def remote_run(host: string, repository: string, target: string, is_sudo: bool):
+    sudo_cmd: string = "sudo -E" if is_sudo else ""
+    cmd = "cd {} && {} make {}".format(repository, sudo_cmd, target)
+    ssh_cmd = "ssh {} \"bash -l -c \'{}\'\"".format(host, cmd)
+    return subprocess.Popen(ssh_cmd, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+# Executes a cleanup command in a remote host.
+def remote_cleanup(host: string, workspace: string, default_branch: string = "master"):
+    cmd = "cd {} && make clean && git checkout {} && git clean -fdx".format(workspace, default_branch)
+    ssh_cmd = "ssh {} \"bash -l -c \'{}\'\"".format(host, cmd)
+    return subprocess.Popen(ssh_cmd, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+# ======================================================================================================================
+# Generic Jobs
+# ======================================================================================================================
+
+
+def job_checkout(repository: string, branch: string, server: string, client: string) -> bool:
+    jobs: list[subprocess.Popen[str]] = []
+    jobs.append(remote_checkout(server, repository, branch))
+    jobs.append(remote_checkout(client, repository, branch))
+    return wait_and_report("checkout", jobs)
+
+
+def job_compile(repository: string, branch: string, libos: string, is_debug: bool, server: string, client: string) -> bool:
+    jobs: list[subprocess.Popen[str]] = []
+    jobs.append(remote_compile(server, repository, "all LIBOS={}".format(libos), is_debug))
+    jobs.append(remote_compile(client, repository, "all LIBOS={}".format(libos), is_debug))
+    return wait_and_report("compile-{}".format("debug" if is_debug else "release"), jobs)
+
+
+def job_test_system_rust(test_name: string, repo: string, libos: string, server: string, client: string,
+                         server_args: string, client_args: string, is_sudo: bool, all_pass: bool) -> bool:
+    server_cmd: string = "test-system-rust LIBOS={} TEST={} ARGS=\\\"{}\\\"".format(libos, test_name, server_args)
+    client_cmd: string = "test-system-rust LIBOS={} TEST={} ARGS=\\\"{}\\\"".format(libos, test_name, client_args)
+    jobs: list[subprocess.Popen[str]] = []
+    jobs.append(remote_run(server, repo, server_cmd, is_sudo))
+    time.sleep(1)
+    jobs.append(remote_run(client, repo, client_cmd, is_sudo))
+    return wait_and_report(test_name, jobs, all_pass)
+
+
+def job_cleanup(repository: string, server: string, client: string) -> bool:
+    jobs: list[subprocess.Popen[str]] = []
+    jobs.append(remote_cleanup(server, repository))
+    jobs.append(remote_cleanup(client, repository))
+    return wait_and_report("cleanup", jobs)
+
+
+# ======================================================================================================================
+# System Test Jobs
+# ======================================================================================================================
+
+def test_udp_ping_pong(
+        server: string, client: string, libos: string, is_sudo: bool, repository: string,
+        server_addr: string, client_addr: string) -> bool:
+    test_name: string = "udp-ping-pong"
+    server_args: string = "--server {}:12345 {}:23456".format(server_addr, client_addr)
+    client_args: string = "--client {}:23456 {}:12345".format(client_addr, server_addr)
+    return job_test_system_rust(test_name, repository, libos, server, client, server_args, client_args, is_sudo, False)
+
+
+def test_udp_push_pop(
+        server: string, client: string, libos: string, is_sudo: bool, repository: string,
+        server_addr: string, client_addr: string) -> bool:
+    test_name: string = "udp-push-pop"
+    server_args: string = "--server {}:12345 {}:23456".format(server_addr, client_addr)
+    client_args: string = "--client {}:23456 {}:12345".format(client_addr, server_addr)
+    return job_test_system_rust(test_name, repository, libos, server, client, server_args, client_args, is_sudo, True)
+
+
+def test_tcp_ping_pong(
+        server: string, client: string, libos: string, is_sudo: bool, repository: string,
+        server_addr: string) -> bool:
+    test_name: string = "tcp-ping-pong"
+    server_args: string = "--server {}:12345".format(server_addr)
+    client_args: string = "--client {}:12345".format(server_addr)
+    return job_test_system_rust(test_name, repository, libos, server, client, server_args, client_args, is_sudo, True)
+
+
+def test_tcp_push_pop(
+        server: string, client: string, libos: string, is_sudo: bool, repository: string,
+        server_addr: string) -> bool:
+    test_name: string = "tcp-push-pop"
+    server_args: string = "--server {}:12345".format(server_addr)
+    client_args: string = "--client {}:12345".format(server_addr)
+    return job_test_system_rust(test_name, repository, libos, server, client, server_args, client_args, is_sudo, True)
+
+# =====================================================================================================================
+
+
+# Runs the CI pipeline.
+def run_pipeline(
+        repository: string, branch: string, libos: string, server: string, client: string, test: bool,
+        server_addr: string, client_addr: string):
+    is_sudo: bool = True if libos == "catnip" or libos == "catpowder" else False
+    passed: bool = False
+
+    # STEP 1: Check out.
+    passed = job_checkout(repository, branch, server, client)
+
+    # STEP 2: Compile debug.
+    if passed:
+        passed = job_compile(repository, branch, libos, True, server, client)
+
+    # STEP 3: Compile release.
+    if passed:
+        passed = job_compile(repository, branch, libos, False, server, client)
+
+    # STEP 4: Run system tests.
+    if test and passed:
+        passed = test_udp_ping_pong(server, client, libos, is_sudo, repository, server_addr, client_addr)
+        passed = test_udp_push_pop(server, client, libos, is_sudo, repository, server_addr, client_addr)
+        passed = test_tcp_ping_pong(server, client, libos, is_sudo, repository, server_addr)
+        passed = test_tcp_push_pop(server, client, libos, is_sudo, repository, server_addr)
+
+    # Setp 5: Clean up.
+    passed = job_cleanup(repository, server, client)
+
+
+# Reads and parses command line arguments.
+def read_args() -> argparse.Namespace:
+    description: string = ""
+    description += "Use this utility to run the regression system of Demikernel on a pair of remote host machines.\n"
+    description += "Before using this utility, ensure that you have correctly setup the development environment on the remote machines.\n"
+    description += "For more information, check out the README.md file of the project."
+
+    # Initialize parser.
+    parser = argparse.ArgumentParser(prog="demikernel-ci.py", description=description)
+
+    # Host options.
+    parser.add_argument("--server", required=True, help="set server host name")
+    parser.add_argument("--client", required=True, help="set client host name")
+
+    # Build options.
+    parser.add_argument("--repository", required=True, help="set location of target repository in remote hosts")
+    parser.add_argument("--branch", required=True, help="set target branch in remote hosts")
+    parser.add_argument("--libos", required=True, help="set target libos in remote hosts")
+
+    # Test options.
+    parser.add_argument("--test", action='store_true', required=False, help="run tests")
+    parser.add_argument("--server-addr", required="--test" in sys.argv, help="sets server address in tests")
+    parser.add_argument("--client-addr", required="--test" in sys.argv, help="sets client address in tests")
+
+    # Read arguments from command line.
+    return parser.parse_args()
+
+
+# Drives the program.
+def main():
+    # Parse and read arguments from command line.
+    args: argparse.Namespace = read_args()
+
+    # Extract host options.
+    server: string = args.server
+    client: string = args.client
+
+    # Extract build options.
+    repository: string = args.repository
+    branch: string = args.branch
+    libos: string = args.libos
+
+    # Extract test options.
+    test: bool = args.test
+    server_addr: string = args.server_addr if test else ""
+    client_addr: string = args.client_addr if test else ""
+
+    run_pipeline(repository, branch, libos, server, client, test, server_addr, client_addr)
+
+
+if __name__ == "__main__":
+    main()
