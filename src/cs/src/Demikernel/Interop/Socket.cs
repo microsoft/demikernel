2fae5caf36ba5d9f8bc6d7b37e860b63240186f2
Marc Gravell <marc.gravell@gmail.com>
2022-12-08 15:18:08 +0000

    add missing file

---
+++ src/cs/src/Demikernel/Interop/Socket.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Demikernel.Interop;
+
+/// <summary>
+/// Represents a Demikernel socket instance
+/// </summary>
+[StructLayout(LayoutKind.Explicit)]
+public readonly struct Socket : IEquatable<Socket>
+{
+    [FieldOffset(0)]
+    internal readonly int Qd;
+
+    /// <summary>Compare two values for equality</summary>
+    public bool Equals(Socket other) => other.Qd == Qd;
+
+    /// <inheritdoc/>
+    public override bool Equals([NotNullWhen(true)] object? obj)
+        => obj is Socket other && other.Qd == Qd;
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => Qd;
+
+    /// <inheritdoc/>
+    public override string ToString() => $"Socket {Qd}";
+
+    /// <summary>Compare two values for equality</summary>
+    public static bool operator ==(Socket x, Socket y) => x.Qd == y.Qd;
+    /// <summary>Compare two values for equality</summary>
+    public static bool operator !=(Socket x, Socket y) => x.Qd != y.Qd;
+}
