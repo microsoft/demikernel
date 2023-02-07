9f987540047ff38c8223985b0be47be1779a1459
Brian Zill <bzill@microsoft.com>
2023-02-07 01:13:48 +0000

    [runtime] Remove unused DataBuffer type.

--- src/rust/runtime/memory/buffer/mod.rs
+++ src/rust/runtime/memory/buffer/mod.rs
@@ -1,91 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-mod databuffer;
 mod demibuffer;
 #[cfg(feature = "libdpdk")]
 mod dpdkbuffer;
 
 //==============================================================================
-// Imports
-//==============================================================================
-
-use ::core::ops::{
-    Deref,
-    DerefMut,
-};
-use ::std::fmt::Debug;
-
-//==============================================================================
 // Exports
 //==============================================================================
 
+pub use self::demibuffer::DemiBuffer;
 #[cfg(feature = "libdpdk")]
 pub use self::dpdkbuffer::DPDKBuffer;
-pub use self::{
-    databuffer::DataBuffer,
-    demibuffer::DemiBuffer,
-};
-
-//==============================================================================
-// Enumerations
-//==============================================================================
-
-#[derive(Clone, Debug)]
-pub enum Buffer {
-    Heap(DataBuffer),
-    #[cfg(feature = "libdpdk")]
-    DPDK(DPDKBuffer),
-}
-
-//==============================================================================
-// Associated Functions
-//==============================================================================
-
-impl Buffer {
-    /// Removes bytes from the front of the target data buffer.
-    pub fn adjust(&mut self, nbytes: usize) {
-        match self {
-            Buffer::Heap(dbuf) => dbuf.adjust(nbytes),
-            #[cfg(feature = "libdpdk")]
-            Buffer::DPDK(mbuf) => mbuf.adjust(nbytes),
-        }
-    }
-
-    /// Removes bytes from the end of the target data buffer.
-    pub fn trim(&mut self, nbytes: usize) {
-        match self {
-            Buffer::Heap(dbuf) => dbuf.trim(nbytes),
-            #[cfg(feature = "libdpdk")]
-            Buffer::DPDK(mbuf) => mbuf.trim(nbytes),
-        }
-    }
-}
-
-//==============================================================================
-// Standard-Library Trait Implementations
-//==============================================================================
-
-/// De-Reference Trait Implementation for Data Buffers
-impl Deref for Buffer {
-    type Target = [u8];
-
-    fn deref(&self) -> &[u8] {
-        match self {
-            Buffer::Heap(dbuf) => dbuf.deref(),
-            #[cfg(feature = "libdpdk")]
-            Buffer::DPDK(mbuf) => mbuf.deref(),
-        }
-    }
-}
-
-/// Mutable De-Reference Trait Implementation for Data Buffers
-impl DerefMut for Buffer {
-    fn deref_mut(&mut self) -> &mut [u8] {
-        match self {
-            Buffer::Heap(dbuf) => dbuf.deref_mut(),
-            #[cfg(feature = "libdpdk")]
-            Buffer::DPDK(mbuf) => mbuf.deref_mut(),
-        }
-    }
-}
