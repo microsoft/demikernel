91e6c57241be301498c3a11092331a2f46a09cff
Irene Zhang <irene.zhang@microsoft.com>
2023-03-01 22:39:39 +0000

    [runtime] Enhancment: new table for per-queue metadata

--- src/rust/runtime/queue/mod.rs
+++ src/rust/runtime/queue/mod.rs
@@ -27,10 +27,13 @@ pub use self::{
 // Structures
 //======================================================================================================================
 
+pub trait IoQueue {
+    fn get_qtype(&self) -> QType;
+}
+
 /// I/O queue descriptors table.
-pub struct IoQueueTable {
-    // TODO: Store a QType in the slab.
-    table: Slab<u32>,
+pub struct IoQueueTable<T: IoQueue> {
+    table: Slab<T>,
 }
 
 //======================================================================================================================
@@ -38,7 +41,7 @@ pub struct IoQueueTable {
 //======================================================================================================================
 
 /// Associated functions for I/O queue descriptors tables.
-impl IoQueueTable {
+impl<T: IoQueue> IoQueueTable<T> {
     /// Offset for I/O queue descriptors.
     ///
     /// When Demikernel is interposing system calls of the underlying OS
@@ -50,12 +53,14 @@ impl IoQueueTable {
 
     /// Creates an I/O queue descriptors table.
     pub fn new() -> Self {
-        Self { table: Slab::new() }
+        Self {
+            table: Slab::<T>::new(),
+        }
     }
 
     /// Allocates a new entry in the target I/O queue descriptors table.
-    pub fn alloc(&mut self, qtype: u32) -> QDesc {
-        let index: usize = self.table.insert(qtype);
+    pub fn alloc(&mut self, queue: T) -> QDesc {
+        let index: usize = self.table.insert(queue);
 
         // Ensure that the allocation would yield to a safe conversion between usize to u32.
         // Note: This imposes a limit on the number of open queue descriptors in u32::MAX.
@@ -67,24 +72,30 @@ impl IoQueueTable {
         QDesc::from((index as u32) + Self::BASE_QD)
     }
 
-    /// Gets the entry associated with an I/O queue descriptor.
-    pub fn get(&self, qd: QDesc) -> Option<u32> {
+    /// Gets/borrows a reference to the queue metadata associated with an I/O queue descriptor.
+    pub fn get(&self, qd: &QDesc) -> Option<&T> {
         let index: u32 = self.get_index(qd)?;
-        self.table.get(index as usize).cloned()
+        self.table.get(index as usize)
+    }
+
+    /// Gets/borrows a mutable reference to the queue metadata associated with an I/O queue descriptor
+    pub fn get_mut(&mut self, qd: &QDesc) -> Option<&mut T> {
+        let index: u32 = self.get_index(qd)?;
+        self.table.get_mut(index as usize)
     }
 
     /// Releases the entry associated with an I/O queue descriptor.
-    pub fn free(&mut self, qd: QDesc) -> Option<u32> {
+    pub fn free(&mut self, qd: &QDesc) -> Option<T> {
         let index: u32 = self.get_index(qd)?;
         Some(self.table.remove(index as usize))
     }
 
     /// Gets the index in the I/O queue descriptors table to which a given I/O queue descriptor refers to.
-    fn get_index(&self, qd: QDesc) -> Option<u32> {
-        if Into::<u32>::into(qd) < Self::BASE_QD {
+    fn get_index(&self, qd: &QDesc) -> Option<u32> {
+        if Into::<u32>::into(*qd) < Self::BASE_QD {
             None
         } else {
-            let rawqd: u32 = Into::<u32>::into(qd) - Self::BASE_QD;
+            let rawqd: u32 = Into::<u32>::into(*qd) - Self::BASE_QD;
             if !self.table.contains(rawqd as usize) {
                 return None;
             }
@@ -99,7 +110,10 @@ impl IoQueueTable {
 
 #[cfg(test)]
 mod tests {
-    use super::IoQueueTable;
+    use super::{
+        IoQueue,
+        IoQueueTable,
+    };
     use crate::{
         QDesc,
         QType,
@@ -109,14 +123,22 @@ mod tests {
         Bencher,
     };
 
+    pub struct TestQueue {}
+
+    impl IoQueue for TestQueue {
+        fn get_qtype(&self) -> QType {
+            QType::TestQueue
+        }
+    }
+
     #[bench]
     fn bench_alloc_free(b: &mut Bencher) {
-        let mut ioqueue_table: IoQueueTable = IoQueueTable::new();
+        let mut ioqueue_table: IoQueueTable<TestQueue> = IoQueueTable::<TestQueue>::new();
 
         b.iter(|| {
-            let qd: QDesc = ioqueue_table.alloc(QType::TcpSocket.into());
+            let qd: QDesc = ioqueue_table.alloc(TestQueue {});
             black_box(qd);
-            let qtype: Option<u32> = ioqueue_table.free(qd);
+            let qtype: Option<TestQueue> = ioqueue_table.free(&qd);
             black_box(qtype);
         });
     }
