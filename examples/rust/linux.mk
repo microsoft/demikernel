3735f82c5c9ad87f8279f6d980ad7a09d1e14f68
Pedro Henrique Penna <ppenna@microsoft.com>
2023-03-16 10:49:51 -0300

    [test] Enhancement: TCP Integration accept()

--- examples/rust/linux.mk
+++ examples/rust/linux.mk
@@ -19,7 +19,6 @@ all: all-examples
 	cp -f $(BUILD_DIR)/examples/tcp-pktgen  $(BINDIR)/examples/rust/tcp-pktgen.$(EXEC_SUFFIX)
 	cp -f $(BUILD_DIR)/examples/tcp-push-pop  $(BINDIR)/examples/rust/tcp-push-pop.$(EXEC_SUFFIX)
 	cp -f $(BUILD_DIR)/examples/tcp-ping-pong $(BINDIR)/examples/rust/tcp-ping-pong.$(EXEC_SUFFIX)
-	cp -f $(BUILD_DIR)/examples/tcp-accept $(BINDIR)/examples/rust/tcp-accept.$(EXEC_SUFFIX)
 	cp -f $(BUILD_DIR)/examples/tcp-close $(BINDIR)/examples/rust/tcp-close.$(EXEC_SUFFIX)
 	cp -f $(BUILD_DIR)/examples/pipe-open $(BINDIR)/examples/rust/pipe-open.$(EXEC_SUFFIX)
 
@@ -41,6 +40,5 @@ clean:
 	@rm -rf $(BINDIR)/examples/rust/tcp-pktgen.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/rust/tcp-push-pop.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/rust/tcp-ping-pong.$(EXEC_SUFFIX)
-	@rm -rf $(BINDIR)/examples/rust/tcp-accept.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/rust/tcp-close.$(EXEC_SUFFIX)
 	@rm -rf $(BINDIR)/examples/rust/pipe-open.$(EXEC_SUFFIX)
