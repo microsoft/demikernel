ce1d346a876646a136e8dc635da16c25f9e1de05
Pedro Henrique Penna <ppenna@microsoft.com>
2023-03-27 11:57:49 -0300

    [test] Enhancement: Attempt to Bind Wildcard Addr
    This commit partially addresses
    https://github.com/demikernel/demikernel/issues/501

--- tests/rust/tcp-test/bind/mod.rs
+++ tests/rust/tcp-test/bind/mod.rs
@@ -43,6 +43,7 @@ pub fn run(libos: &mut LibOS, local: &Ipv4Addr, remote: &Ipv4Addr) -> Result<()>
     bind_same_address_to_two_sockets(libos, local)?;
     bind_to_private_ports(libos, local)?;
     bind_to_wildcard_port(libos, local)?;
+    bind_to_wildcard_address(libos)?;
     bind_to_non_local_address(libos, remote)?;
     bind_to_closed_socket(libos, local)?;
 
@@ -173,6 +174,28 @@ fn bind_to_wildcard_port(libos: &mut LibOS, ipv4: &Ipv4Addr) -> Result<()> {
 
     // Bind socket.
     let addr: SocketAddrV4 = SocketAddrV4::new(*ipv4, 0);
+    libos.bind(sockqd, addr)?;
+
+    // Close socket.
+    libos.close(sockqd)?;
+
+    Ok(())
+}
+
+/// Attempts to bind to the wildcard address.
+fn bind_to_wildcard_address(libos: &mut LibOS) -> Result<()> {
+    println!("{}", stringify!(bind_to_wildcard_address));
+
+    // Create a TCP socket.
+    let sockqd: QDesc = libos.socket(AF_INET, SOCK_STREAM, 0)?;
+
+    // Bind address.
+    let addr: SocketAddrV4 = {
+        let port: u16 = 8080;
+        SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port)
+    };
+
+    // Bind socket.
     libos.bind(sockqd, addr)?;
 
     // Close socket.
