ef2dd3dce1c466ee0cbbadd64e2f0052a4da3e6e
Pedro Henrique Penna <ppenna@microsoft.com>
2023-03-27 14:37:17 -0300

    [test] Enhancement: Listen Closed Socket
    This commit addresses
    https://github.com/demikernel/demikernel/issues/566

--- tests/rust/tcp-test/listen/mod.rs
+++ tests/rust/tcp-test/listen/mod.rs
@@ -47,6 +47,7 @@ pub fn run(libos: &mut LibOS, addr: &SocketAddrV4) -> Result<()> {
     listen_listening_socket(libos, addr)?;
     listen_connecting_socket(libos, addr)?;
     listen_accepting_socket(libos, addr)?;
+    listen_closed_socket(libos, addr)?;
 
     Ok(())
 }
@@ -223,6 +224,31 @@ fn listen_accepting_socket(libos: &mut LibOS, local: &SocketAddrV4) -> Result<()
 
     // Succeed to close socket.
     libos.close(sockqd)?;
+
+    Ok(())
+}
+
+/// Attempts to listen for connections on a TCP socket that is closed.
+fn listen_closed_socket(libos: &mut LibOS, local: &SocketAddrV4) -> Result<()> {
+    println!("{}", stringify!(listen_closed_socket));
+
+    // Create a bound socket.
+    let sockqd: QDesc = libos.socket(AF_INET, SOCK_STREAM, 0)?;
+    libos.bind(sockqd, local.to_owned())?;
+
+    // Succeed to listen().
+    libos.listen(sockqd, 16)?;
+
+    // Succeed to close socket.
+    libos.close(sockqd)?;
+
+    // Fail to listen().
+    let e: Fail = libos
+        .listen(sockqd, 16)
+        .expect_err("listen() on a socket that is closed should fail");
+
+    // Sanity check error code.
+    assert_eq!(e.errno, libc::EBADF, "listen() failed with {}", e.cause);
 
     Ok(())
 }
