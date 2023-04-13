879242709675ccee6147699efd0455d65a7eb809
Pedro Henrique Penna <ppenna@microsoft.com>
2023-04-13 09:40:26 -0300

    [test] Enhancement: Helper Macros for Tests

--- tests/rust/pipe-test/main.rs
+++ tests/rust/pipe-test/main.rs
@@ -23,6 +23,44 @@ use ::demikernel::{
 };
 
 //======================================================================================================================
+// Macros
+//======================================================================================================================
+
+/// Runs a test and prints if it passed or failed on the standard output.
+#[macro_export]
+macro_rules! test {
+    ($fn_name:ident($($arg:expr),*)) => {{
+        match $fn_name($($arg),*) {
+            Ok(ok) =>
+                vec![(stringify!($fn_name).to_string(), "passed".to_string(), Ok(ok))],
+            Err(err) =>
+                vec![(stringify!($fn_name).to_string(), "failed".to_string(), Err(err))],
+        }
+    }};
+}
+
+/// Updates the error variable `err_var` with `anyhow::anyhow!(msg)` if `err_var` is `Ok`.
+/// Otherwise it prints `msg` on the standard output.
+#[macro_export]
+macro_rules! update_error {
+    ($err_var:ident, $msg:expr) => {
+        if $err_var.is_ok() {
+            $err_var = Err(anyhow::anyhow!($msg));
+        } else {
+            println!("{}", $msg);
+        }
+    };
+}
+
+/// Collects the result of a test and appends it to a vector.
+#[macro_export]
+macro_rules! collect {
+    ($vec:ident, $expr:expr) => {
+        $vec.append(&mut $expr);
+    };
+}
+
+//======================================================================================================================
 // Standalone Functions
 //======================================================================================================================
 
