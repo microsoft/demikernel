710398098b2203233007fcd0caa47324d5e9c0d1
Pedro Henrique Penna <ppenna@microsoft.com>
2023-04-13 14:28:19 -0300

    [test] Enhancement: Close Pipe Multiple Times
    This commit partially addresses
    https://github.com/demikernel/demikernel/issues/617

--- tests/rust/pipe-test/close/mod.rs
+++ tests/rust/pipe-test/close/mod.rs
@@ -20,6 +20,7 @@ pub fn run(libos: &mut LibOS, pipe_name: &str) -> Vec<(String, String, Result<()
     let mut result: Vec<(String, String, Result<(), anyhow::Error>)> = Vec::new();
 
     crate::collect!(result, crate::test!(close_invalid_pipe(libos)));
+    crate::collect!(result, crate::test!(close_pipe_multiple_times(libos, pipe_name)));
 
     result
 }
@@ -41,4 +42,29 @@ fn close_invalid_pipe(libos: &mut LibOS) -> Result<()> {
     };
 
     Ok(())
+}
+
+/// Attempts to close the same pipe multiple times.
+fn close_pipe_multiple_times(libos: &mut LibOS, pipe_name: &str) -> Result<()> {
+    // Create a pipe.
+    let pipeqd: QDesc = match libos.create_pipe(pipe_name) {
+        Ok(pipeqd) => pipeqd,
+        Err(e) => anyhow::bail!("create_pipe() failed ({})", e),
+    };
+
+    // Succeed to close the pipe.
+    match libos.close(pipeqd) {
+        Ok(_) => (),
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("close() failed ({})", e);
+        },
+    };
+
+    // Fail to close the pipe again.
+    match libos.close(pipeqd) {
+        Err(e) if e.errno == libc::EBADF => Ok(()),
+        Ok(_) => anyhow::bail!("close() invalid pipe should fail"),
+        Err(e) => anyhow::bail!("close() failed ({})", e),
+    }
 }
