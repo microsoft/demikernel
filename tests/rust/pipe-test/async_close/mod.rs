6fb5c0af69c38470156ac012c8bb6d57960cb9c2
Pedro Henrique Penna <ppenna@microsoft.com>
2023-04-14 09:54:14 -0300

    [test] Enhancement: Async Close Pipe Twice
    This commit partially addresses
    https://github.com/demikernel/demikernel/issues/620

--- tests/rust/pipe-test/async_close/mod.rs
+++ tests/rust/pipe-test/async_close/mod.rs
@@ -7,9 +7,12 @@
 
 use ::anyhow::Result;
 use ::demikernel::{
+    runtime::types::demi_opcode_t,
     LibOS,
     QDesc,
+    QToken,
 };
+use ::std::time::Duration;
 
 //======================================================================================================================
 // Standalone Functions
@@ -20,6 +23,18 @@ pub fn run(libos: &mut LibOS, pipe_name: &str) -> Vec<(String, String, Result<()
     let mut result: Vec<(String, String, Result<(), anyhow::Error>)> = Vec::new();
 
     crate::collect!(result, crate::test!(async_close_invalid_pipe(libos)));
+    crate::collect!(
+        result,
+        crate::test!(async_close_pipe_multiple_times_1(libos, pipe_name))
+    );
+    crate::collect!(
+        result,
+        crate::test!(async_close_pipe_multiple_times_2(libos, pipe_name))
+    );
+    crate::collect!(
+        result,
+        crate::test!(async_close_pipe_multiple_times_3(libos, pipe_name))
+    );
 
     result
 }
@@ -41,4 +56,165 @@ fn async_close_invalid_pipe(libos: &mut LibOS) -> Result<()> {
     };
 
     Ok(())
+}
+
+/// Attempts to asynchronously close the same pipe multiple times.
+fn async_close_pipe_multiple_times_1(libos: &mut LibOS, pipe_name: &str) -> Result<()> {
+    // Create a pipe.
+    let pipeqd: QDesc = match libos.create_pipe(pipe_name) {
+        Ok(pipeqd) => pipeqd,
+        Err(e) => anyhow::bail!("create_pipe() failed ({})", e),
+    };
+
+    // Succeed to issue asynchronous close.
+    let qt: QToken = match libos.async_close(pipeqd) {
+        Ok(qt) => qt,
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("async_close() failed ({})", e);
+        },
+    };
+
+    // Poll once to ensure the async_close() coroutine runs and finishes the close.
+    match libos.wait(qt, Some(Duration::from_micros(0))) {
+        Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_CLOSE && qr.qr_ret == 0 => {},
+        Err(e) if e.errno == libc::ETIMEDOUT => {
+            anyhow::bail!("wait() async_close() should not timeout")
+        },
+        Ok(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+        Err(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+    }
+
+    // Fail to close the pipe again.
+    match libos.async_close(pipeqd) {
+        Err(e) if e.errno == libc::EBADF => Ok(()),
+        Ok(_) => anyhow::bail!("async_close() a pipe multiple times should fail"),
+        Err(e) => anyhow::bail!("async_close() failed ({})", e),
+    }
+}
+
+/// Attempts to asynchronously close the same pipe multiple times.
+fn async_close_pipe_multiple_times_2(libos: &mut LibOS, pipe_name: &str) -> Result<()> {
+    let mut cancelled: bool = false;
+    let mut closed: bool = false;
+
+    // Create a pipe.
+    let pipeqd: QDesc = match libos.create_pipe(pipe_name) {
+        Ok(pipeqd) => pipeqd,
+        Err(e) => anyhow::bail!("create_pipe() failed ({})", e),
+    };
+
+    // Succeed to issue asynchronous close.
+    let qt1: QToken = match libos.async_close(pipeqd) {
+        Ok(qt) => qt,
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("async_close() failed ({})", e);
+        },
+    };
+
+    // Succeed to issue asynchronous close.
+    let qt2: QToken = match libos.async_close(pipeqd) {
+        Ok(qt) => qt,
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("async_close() failed ({})", e);
+        },
+    };
+
+    // Poll once to ensure the async_close() coroutine runs and finishes the close.
+    match libos.wait(qt1, Some(Duration::from_micros(0))) {
+        Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_CLOSE && qr.qr_ret == 0 => closed = true,
+        Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::ECANCELED => cancelled = true,
+        Ok(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+        Err(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+    }
+
+    // Poll once to ensure the async_close() coroutine runs and finishes the close.
+    match libos.wait(qt2, Some(Duration::from_micros(0))) {
+        Ok(qr) => {
+            if cancelled && qr.qr_opcode == demi_opcode_t::DEMI_OPC_CLOSE && qr.qr_ret == 0 {
+                return Ok(());
+            }
+            if closed && qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::EBADF {
+                return Ok(());
+            }
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+        Err(_) => {
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+    }
+}
+
+/// Attempts to asynchronously close the same pipe multiple times.
+fn async_close_pipe_multiple_times_3(libos: &mut LibOS, pipe_name: &str) -> Result<()> {
+    let mut cancelled: bool = false;
+    let mut closed: bool = false;
+
+    // Create a pipe.
+    let pipeqd: QDesc = match libos.create_pipe(pipe_name) {
+        Ok(pipeqd) => pipeqd,
+        Err(e) => anyhow::bail!("create_pipe() failed ({})", e),
+    };
+
+    // Succeed to issue asynchronous close.
+    let qt1: QToken = match libos.async_close(pipeqd) {
+        Ok(qt) => qt,
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("async_close() failed ({})", e);
+        },
+    };
+
+    // Succeed to issue asynchronous close.
+    let qt2: QToken = match libos.async_close(pipeqd) {
+        Ok(qt) => qt,
+        Err(e) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("async_close() failed ({})", e);
+        },
+    };
+
+    // Poll once to ensure the async_close() coroutine runs and finishes the close.
+    match libos.wait(qt2, Some(Duration::from_micros(0))) {
+        Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_CLOSE && qr.qr_ret == 0 => closed = true,
+        Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::ECANCELED => cancelled = true,
+        Ok(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+        Err(_) => {
+            println!("[ERROR] leaking pipeqd={:?}", pipeqd);
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+    }
+
+    // Poll once to ensure the async_close() coroutine runs and finishes the close.
+    match libos.wait(qt1, Some(Duration::from_micros(0))) {
+        Ok(qr) => {
+            if cancelled && qr.qr_opcode == demi_opcode_t::DEMI_OPC_CLOSE && qr.qr_ret == 0 {
+                return Ok(());
+            }
+            if closed && qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::EBADF {
+                return Ok(());
+            }
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+        Err(_) => {
+            anyhow::bail!("wait() should succeed with async_close()")
+        },
+    }
 }
