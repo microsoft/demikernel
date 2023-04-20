455997efad65421e1842ba8a56f258cccf868436
Pedro Henrique Penna <ppenna@microsoft.com>
2023-04-20 15:56:17 -0300

    [test] Enhancement: Wait Pop After Closing Pipe
    This commit partially addresses
    https://github.com/demikernel/demikernel/issues/619

---
+++ tests/rust/pipe-test/pop_wait/client.rs
@@ -0,0 +1,121 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+//======================================================================================================================
+// Imports
+//======================================================================================================================
+
+use ::anyhow::Result;
+use ::demikernel::{
+    demi_sgarray_t,
+    runtime::types::demi_opcode_t,
+    LibOS,
+    QDesc,
+    QToken,
+};
+use ::std::slice;
+use std::time::Duration;
+
+//======================================================================================================================
+// Structures
+//======================================================================================================================
+
+/// Pipe client
+pub struct PipeClient {
+    /// Underlying libOS.
+    libos: LibOS,
+    /// Underlying pipe descriptor.
+    pipeqd: QDesc,
+}
+
+//======================================================================================================================
+// Associated Functions
+//======================================================================================================================
+
+impl PipeClient {
+    /// Creates a new pipe client.
+    pub fn new(mut libos: LibOS, pipe_name: String) -> Result<Self> {
+        let pipeqd: QDesc = libos.open_pipe(&format!("{}:rx", pipe_name))?;
+        Ok(Self { libos, pipeqd })
+    }
+
+    // Runs the target pipe client.
+    pub fn run(&mut self) -> Result<()> {
+        // Push data while it is non-blocking for 1 second, then assume that
+        // the server has closed its end of the pipe and stop sending.
+        loop {
+            // Push scatter-gather array.
+            let qt: QToken = self.push_and_dont_wait()?;
+
+            // Wait for operation to complete or timeout.
+            match self.libos.wait(qt, Some(Duration::from_secs(1))) {
+                Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_PUSH && qr.qr_ret == 0 => {},
+                Err(e) if e.errno == libc::ETIMEDOUT => break,
+                Ok(_) => {
+                    anyhow::bail!("wait() should not complete successfully with an opcode other than DEMI_OPC_PUSH")
+                },
+                Err(e) => anyhow::bail!("wait() should not fail (error={:?})", e),
+            }
+        }
+
+        Ok(())
+    }
+
+    // Pushes a scatter-gather array, but does not wait for the operation to complete.
+    fn push_and_dont_wait(&mut self) -> Result<QToken> {
+        // Allocate a scatter-gather array. Length and value are arbitrary.
+        let sga: demi_sgarray_t = match self.mksga(1, 123) {
+            Ok(sga) => sga,
+            Err(e) => anyhow::bail!("mksga() failed (error={:?})", e),
+        };
+
+        // Push scatter-gather array.
+        let qt: Result<QToken> = match self.libos.push(self.pipeqd, &sga) {
+            Ok(qt) => Ok(qt),
+            Err(e) => Err(anyhow::anyhow!("push() failed (error={:?})", e)),
+        };
+
+        // Succeed to release scatter-gather-array.
+        if let Err(e) = self.libos.sgafree(sga) {
+            println!("WARN: leaking sga");
+            println!("ERROR: sgafree() failed (error={:?})", e);
+        }
+
+        qt
+    }
+
+    /// Makes a scatter-gather array.
+    fn mksga(&mut self, size: usize, value: u8) -> Result<demi_sgarray_t> {
+        // Allocate scatter-gather array.
+        let sga: demi_sgarray_t = match self.libos.sgaalloc(size) {
+            Ok(sga) => sga,
+            Err(e) => anyhow::bail!("failed to allocate scatter-gather array: {:?}", e),
+        };
+
+        // Ensure that scatter-gather array has the requested size.
+        demikernel::ensure_eq!(sga.sga_segs[0].sgaseg_len as usize, size);
+
+        // Fill in scatter-gather array.
+        let ptr: *mut u8 = sga.sga_segs[0].sgaseg_buf as *mut u8;
+        let len: usize = sga.sga_segs[0].sgaseg_len as usize;
+        let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ptr, len) };
+        slice.fill(value);
+
+        Ok(sga)
+    }
+}
+
+//======================================================================================================================
+// Trait Implementations
+//======================================================================================================================
+
+impl Drop for PipeClient {
+    // Releases all resources allocated to a pipe client.
+    fn drop(&mut self) {
+        // Ignore error.
+        if let Err(e) = self.libos.close(self.pipeqd) {
+            println!("WARN: leaking pipeqd={:?}", self.pipeqd);
+            println!("ERROR: close() failed (error={:?})", e);
+        }
+    }
+}
---
+++ tests/rust/pipe-test/pop_wait/server.rs
@@ -0,0 +1,159 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+//======================================================================================================================
+// Imports
+//======================================================================================================================
+
+use ::anyhow::Result;
+use ::demikernel::{
+    demi_sgarray_t,
+    runtime::types::{
+        demi_opcode_t,
+        demi_qresult_t,
+    },
+    LibOS,
+    QDesc,
+    QToken,
+};
+use ::std::time::Duration;
+
+//======================================================================================================================
+// Structures
+//======================================================================================================================
+
+/// Pipe server
+pub struct PipeServer {
+    /// Underlying libOS.
+    libos: LibOS,
+    /// Underlying pipe descriptor.
+    pipeqd: Option<QDesc>,
+}
+
+//======================================================================================================================
+// Associated Functions
+//======================================================================================================================
+
+impl PipeServer {
+    /// Creates a new pipe server.
+    pub fn new(mut libos: LibOS, pipe_name: String) -> Result<Self> {
+        let pipeqd: QDesc = libos.create_pipe(&format!("{}:rx", pipe_name))?;
+        Ok(Self {
+            libos,
+            pipeqd: Some(pipeqd),
+        })
+    }
+
+    /// Runs the target pipe server.
+    pub fn run(&mut self) -> Result<()> {
+        let mut pop_completed: bool = false;
+
+        // Succeed to pop some data.
+        // The number of pops is set to an arbitrary value.
+        for _ in 0..16 {
+            self.pop_and_wait()?;
+        }
+
+        // Succeed to pop.
+        let qt: QToken = self.pop_and_dont_wait()?;
+
+        // Poll once to ensure that the co-routine runs.
+        match self.libos.wait(qt, Some(Duration::from_micros(0))) {
+            Err(e) if e.errno == libc::ETIMEDOUT => {},
+            Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_POP && qr.qr_ret == 0 => {
+                pop_completed = true;
+                self.handle_pop(&qr)?;
+            },
+            Ok(_) => anyhow::bail!("wait() should complete successfully"),
+            Err(e) => anyhow::bail!("wait() should fail with ETIMEDOUT (error={:?})", e),
+        }
+
+        // Succeed to close pipe.
+        // The following call to except() is safe because pipeqd is ensured to be open and assigned Some() value.
+        match self.libos.close(self.pipeqd.expect("pipe should not be closed")) {
+            Ok(()) => self.pipeqd = None,
+            Err(e) => anyhow::bail!("close() failed (error={:?})", e),
+        }
+
+        // Wait for operation to complete.
+        if !pop_completed {
+            match self.libos.wait(qt, None) {
+                Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::ECANCELED => Ok(()),
+                Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_POP && qr.qr_ret == 0 => self.handle_pop(&qr),
+                Ok(_) => {
+                    anyhow::bail!("wait() should either complete successfully or fail with ECANCELED")
+                },
+                Err(e) => anyhow::bail!("wait() should failed (error={:?})", e),
+            }
+        } else {
+            match self.libos.wait(qt, None) {
+                Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_FAILED && qr.qr_ret == libc::ECANCELED => Ok(()),
+                Ok(_) => anyhow::bail!("wait() should fail with ECANCELLED"),
+                Err(e) => anyhow::bail!("wait() failed (error={:?})", e),
+            }
+        }
+    }
+
+    /// Pops a scatter-gather array, but does not wait for the operation to complete.
+    fn pop_and_dont_wait(&mut self) -> Result<QToken> {
+        // The following call to except() is safe because pipeqd is ensured to be open and assigned Some() value.
+        match self.libos.pop(self.pipeqd.expect("pipe should not be closed"), None) {
+            Ok(qt) => Ok(qt),
+            Err(e) => anyhow::bail!("pop() failed (error={:?})", e),
+        }
+    }
+
+    /// Pops a scatter-gather array and waits for the operation to complete.
+    fn pop_and_wait(&mut self) -> Result<()> {
+        let qt: QToken = self.pop_and_dont_wait()?;
+
+        // Wait for operation to complete.
+        match self.libos.wait(qt, None) {
+            Ok(qr) if qr.qr_opcode == demi_opcode_t::DEMI_OPC_POP && qr.qr_ret == 0 => {
+                let sga: demi_sgarray_t = unsafe { qr.qr_value.sga };
+
+                // Succeed to release scatter-gather array.
+                match self.libos.sgafree(sga) {
+                    Ok(()) => Ok(()),
+                    Err(e) => {
+                        println!("WARN: leaking sga");
+                        anyhow::bail!("sgafree() failed (error={:?})", e);
+                    },
+                }
+            },
+            Ok(_) => anyhow::bail!("wait() should not complete successfully with an opcode other than DEMI_OPC_POP"),
+            Err(e) => anyhow::bail!("wait() should not fail (error={:?})", e),
+        }
+    }
+
+    /// Handles the completion of a pop operation.
+    fn handle_pop(&mut self, qr: &demi_qresult_t) -> Result<()> {
+        let sga: demi_sgarray_t = unsafe { qr.qr_value.sga };
+
+        // Succeed to release scatter-gather array.
+        match self.libos.sgafree(sga) {
+            Ok(()) => Ok(()),
+            Err(e) => {
+                println!("WARN: leaking sga");
+                anyhow::bail!("sgafree() failed (error={:?})", e);
+            },
+        }
+    }
+}
+
+//======================================================================================================================
+// Trait Implementations
+//======================================================================================================================
+
+impl Drop for PipeServer {
+    // Releases all resources allocated to a pipe server.
+    fn drop(&mut self) {
+        if let Some(pipeqd) = self.pipeqd {
+            // Ignore error.
+            if let Err(e) = self.libos.close(pipeqd) {
+                println!("ERROR: close() failed (error={:?})", e);
+                println!("WARN: leaking pipeqd={:?}", pipeqd);
+            }
+        }
+    }
+}
