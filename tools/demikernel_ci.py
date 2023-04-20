819e7630ca8a8cb635f7cf195577a67f83210ba1
Pedro Henrique Penna <ppenna@microsoft.com>
2023-04-20 08:48:12 -0300

    [ci] Enhancement: Enable PROFILER Flag on Compile
    This commit addresses
    https://github.com/demikernel/demikernel/issues/675

--- tools/demikernel_ci.py
+++ tools/demikernel_ci.py
@@ -76,7 +76,7 @@ def remote_checkout(host: str, repository: str, branch: str):
 # Executes a compile command in a remote host.
 def remote_compile(host: str, repository: str, target: str, is_debug: bool):
     debug_flag: str = "DEBUG=yes" if is_debug else "DEBUG=no"
-    cmd = "cd {} && make {} {}".format(repository, debug_flag, target)
+    cmd = "cd {} && make PROFILER=yes {} {}".format(repository, debug_flag, target)
     ssh_cmd = "ssh {} \"bash -l -c \'{}\'\"".format(host, cmd)
     return subprocess.Popen(ssh_cmd, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
