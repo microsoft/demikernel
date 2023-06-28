61a20569b89ae6278bf5b359e189f63631f5ada7
Pedro Henrique Penna <ppenna@microsoft.com>
2023-06-28 08:44:38 -0300

    [catmem] Enhancement: Pipe States

--- src/rust/catmem/pipe.rs
+++ src/rust/catmem/pipe.rs
@@ -12,8 +12,21 @@ use ::std::rc::Rc;
 // Structures
 //======================================================================================================================
 
+/// Encodes the states of a pipe.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum PipeState {
+    /// A pipe that is opened.
+    Opened,
+    /// A pipe that is closing.
+    Closing,
+    /// A pipe that is closed.
+    Closed,
+}
+
 /// A pipe.
 pub struct Pipe {
+    /// State of the pipe.
+    state: PipeState,
     /// Indicates end of file.
     eof: bool,
     /// Underlying buffer.
@@ -28,6 +41,7 @@ impl Pipe {
     /// Creates a new pipe.
     pub fn new(buffer: SharedRingBuffer<u16>) -> Self {
         Self {
+            state: PipeState::Opened,
             eof: false,
             buffer: Rc::new(buffer),
         }
@@ -41,6 +55,16 @@ impl Pipe {
     /// Gets the value of he end of file flag in the target pipe.
     pub fn eof(&self) -> bool {
         self.eof
+    }
+
+    /// Gets the state of the pipe.
+    pub fn state(&self) -> PipeState {
+        self.state
+    }
+
+    /// Sets the state of the pipe.
+    pub fn set_state(&mut self, state: PipeState) {
+        self.state = state;
     }
 
     /// Gets a reference to the underlying buffer of the target pipe.
