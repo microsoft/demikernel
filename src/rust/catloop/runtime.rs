abfa27cb36d91378c64c06c3a44fc34177f5cbc2
Pedro Henrique Penna <ppenna@microsoft.com>
2023-08-14 06:31:14 -0300

    [catloop] Enhancement: Improving Documentation

--- src/rust/catloop/runtime.rs
+++ src/rust/catloop/runtime.rs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//==============================================================================
+//======================================================================================================================
 // Imports
-//==============================================================================
+//======================================================================================================================
 
 use crate::{
     catloop::{
@@ -31,19 +31,20 @@ use ::std::{
     collections::HashMap,
     net::SocketAddrV4,
 };
-//==============================================================================
+
+//======================================================================================================================
 // Structures
-//==============================================================================
+//======================================================================================================================
 
 /// Catloop Runtime
 pub struct CatloopRuntime {
     /// Ephemeral port allocator.
     ephemeral_ports: EphemeralPorts,
-    /// Table of queue descriptors. This table has one entry for each existing queue descriptor in Catloop LibOS.
+    /// Table of queue descriptors, it has one entry for each existing queue descriptor in Catloop LibOS.
     qtable: IoQueueTable<CatloopQueue>,
-    /// Table for ongoing operations.
+    /// Table for ongoing operations on Catloop LibOS.
     catloop_qts: HashMap<QToken, (demi_opcode_t, QDesc)>,
-    /// Table for ongoing operations.
+    /// Table for ongoing operations Catmem LibOS.
     catmem_qts: HashMap<QToken, (demi_opcode_t, QDesc)>,
 }
 
@@ -63,7 +64,7 @@ impl CatloopRuntime {
         }
     }
 
-    /// This function allocates a new Catloop queue of [qtype].
+    /// Allocates a new [CatloopQueue] of `qtype`.
     pub fn alloc_queue(&mut self, qtype: QType) -> QDesc {
         self.qtable.alloc(CatloopQueue::new(qtype))
     }
@@ -72,8 +73,7 @@ impl CatloopRuntime {
         self.qtable.free(&qd);
     }
 
-    /// Get the CatloopQueue associated with this [qd]. If not a valid queue, then return EBADF with "invalid queue
-    /// descriptor".
+    /// Gets the [CatloopQueue] associated with `qd`. If not `qd` does not refer to a valid, then return `EBADF` is returned.
     pub fn get_queue(&mut self, qd: QDesc) -> Result<&mut CatloopQueue, Fail> {
         match self.qtable.get_mut(&qd) {
             Some(queue) => Ok(queue),
@@ -85,37 +85,37 @@ impl CatloopRuntime {
         }
     }
 
-    /// This function inserts a given queue token into the Catloop queue token table.
+    /// Inserts a given `qt` queue token into the Catloop queue token table.
     pub fn insert_catloop_qt(&mut self, qt: QToken, opcode: demi_opcode_t, qd: QDesc) {
         self.catloop_qts.insert(qt, (opcode, qd));
     }
 
-    /// This function inserts a given queue token into the Catmem queue token table.
+    /// Inserts a given `qt` into the Catmem queue token table.
     pub fn insert_catmem_qt(&mut self, qt: QToken, opcode: demi_opcode_t, qd: QDesc) {
         self.catmem_qts.insert(qt, (opcode, qd));
     }
 
-    /// This function gets all current queue tokens in the Catloop queue token table.
+    /// Gets the [QDesc] associated with `qt` in the Catloop queue token table.
     pub fn get_catloop_qd(&self, qt: QToken) -> Option<&(demi_opcode_t, QDesc)> {
         self.catloop_qts.get(&qt)
     }
 
-    /// This function gets all current queue tokens in the Catmem queue token table.
+    /// Gets the [QDesc] associated with `qt` in the Catmem queue token table.
     pub fn get_catmem_qd(&self, qt: QToken) -> Option<&(demi_opcode_t, QDesc)> {
         self.catmem_qts.get(&qt)
     }
 
-    /// This function gets all current queue tokens in the Catloop queue token table.
+    /// Removes `qt` from the  Catloop queue token table.
     pub fn free_catloop_qt(&mut self, qt: QToken) -> Option<(demi_opcode_t, QDesc)> {
         self.catloop_qts.remove(&qt)
     }
 
-    /// This function gets all current queue tokens in the Catmem queue token table.
+    /// Removes `qt` from the Catmem queue token table.
     pub fn free_catmem_qt(&mut self, qt: QToken) -> Option<(demi_opcode_t, QDesc)> {
         self.catmem_qts.remove(&qt)
     }
 
-    /// This function checks whether [local] is already bound to a queue. Returns true if not bound and false if
+    /// Checks whether `local` is bound to `addr`. On successful completion it returns `true` if not bound and `false` if
     /// already in use.
     pub fn is_bound_to_addr(&self, local: SocketAddrV4) -> bool {
         for (_, queue) in self.qtable.get_values() {
@@ -127,7 +127,7 @@ impl CatloopRuntime {
         true
     }
 
-    /// This function allocates an ephemeral port.
+    /// Allocates an ephemeral port. If `port` is `Some(port)` then it tries to allocate `port`.
     pub fn alloc_ephemeral_port(&mut self, port: Option<u16>) -> Result<Option<u16>, Fail> {
         if let Some(port) = port {
             self.ephemeral_ports.alloc_port(port)?;
@@ -137,20 +137,21 @@ impl CatloopRuntime {
         }
     }
 
+    /// Releases an ephemeral `port`.
     pub fn free_ephemeral_port(&mut self, port: u16) -> Result<(), Fail> {
         self.ephemeral_ports.free(port)
     }
 }
 
-//==============================================================================
+//======================================================================================================================
 // Trait Implementations
-//==============================================================================
+//======================================================================================================================
 
 /// Memory Runtime Trait Implementation for Catloop Runtime
 impl MemoryRuntime for CatloopRuntime {}
 
 impl Drop for CatloopRuntime {
-    // Releases all sockets allocated by Catnap.
+    /// Releases all resources allocated by Catloop.
     fn drop(&mut self) {
         for (_, queue) in self.qtable.get_values() {
             if let Some(duplex_pipe) = queue.get_pipe() {
