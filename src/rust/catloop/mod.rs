3dd027c66f81d5f9554009ac2463dbef752fcd78
Pedro Henrique Penna <ppenna@microsoft.com>
2023-08-22 14:16:14 -0300

    [catloop] Workaround: Disallow Bind on Wildcard Port

--- src/rust/catloop/mod.rs
+++ src/rust/catloop/mod.rs
@@ -143,6 +143,7 @@ impl CatloopLibOS {
         }
 
         // Check if we are binding to the wildcard port.
+        // FIXME: https://github.com/demikernel/demikernel/issues/582
         if local.port() == 0 {
             let cause: String = format!("cannot bind to port 0 (qd={:?})", qd);
             error!("bind(): {}", cause);
