de8a402f744b23746f223bcdda1739ee4ec043bc
Pedro Henrique Penna <ppenna@microsoft.com>
2023-08-29 13:47:54 -0300

    [catmem] Enhancement: Squashing Try Push

--- src/rust/catmem/queue.rs
+++ src/rust/catmem/queue.rs
@@ -221,17 +221,13 @@ impl CatmemQueue {
         self.do_generic_sync_data_path_call(insert_coroutine)
     }
 
-    /// This private function tries to push a single byte and is used for scoping the borrow.
-    fn try_push(&self, byte: &u8) -> Result<bool, Fail> {
-        self.ring.borrow_mut().try_push(byte)
-    }
-
     /// This function tries to push [buf] to the shared memory ring. If the queue is connected to the pop end, then
     /// this function returns an error.
     pub async fn do_push(&self, buf: DemiBuffer, yielder: Yielder) -> Result<(), Fail> {
         for byte in &buf[..] {
             loop {
-                match self.try_push(byte)? {
+                let push_result: bool = self.ring.borrow_mut().try_push(byte)?;
+                match push_result {
                     true => break,
                     false => {
                         // Operation not completed. Check if it was cancelled.
