443de6fddd4a35025c0c0eba9ee4fb6210ae9cea
Anand Bonde <anand.bonde@gmail.com>
2023-11-07 22:43:12 +0000

    [catnip] Enhancement: pack_result parity

--- src/rust/catnip/interop.rs
+++ src/rust/catnip/interop.rs
@@ -3,14 +3,7 @@
 
 use crate::{
     catnip::SharedDPDKRuntime,
-    pal::{
-        constants::AF_INET,
-        data_structures::{
-            SockAddr,
-            SockAddrIn,
-        },
-        functions::create_sin_addr,
-    },
+    pal::data_structures::SockAddr,
     runtime::{
         memory::MemoryRuntime,
         types::{
@@ -25,6 +18,12 @@ use crate::{
 };
 use ::std::mem;
 
+#[cfg(target_os = "linux")]
+use crate::pal::linux::socketaddrv4_to_sockaddr;
+
+#[cfg(target_os = "windows")]
+use crate::pal::functions::socketaddrv4_to_sockaddr;
+
 pub fn pack_result(rt: &SharedDPDKRuntime, result: OperationResult, qd: QDesc, qt: u64) -> demi_qresult_t {
     match result {
         OperationResult::Connect => demi_qresult_t {
@@ -35,18 +34,11 @@ pub fn pack_result(rt: &SharedDPDKRuntime, result: OperationResult, qd: QDesc, q
             qr_value: unsafe { mem::zeroed() },
         },
         OperationResult::Accept((new_qd, addr)) => {
-            let saddr: SockAddrIn = {
-                SockAddrIn {
-                    sin_family: AF_INET,
-                    sin_port: addr.port().into(),
-                    sin_addr: create_sin_addr(&addr.ip().octets()),
-                    sin_zero: [0; 8],
-                }
-            };
+            let saddr: SockAddr = socketaddrv4_to_sockaddr(&addr);
             let qr_value: demi_qr_value_t = demi_qr_value_t {
                 ares: demi_accept_result_t {
                     qd: new_qd.into(),
-                    addr: unsafe { mem::transmute::<SockAddrIn, SockAddr>(saddr) },
+                    addr: saddr,
                 },
             };
             demi_qresult_t {
@@ -66,18 +58,10 @@ pub fn pack_result(rt: &SharedDPDKRuntime, result: OperationResult, qd: QDesc, q
         },
         OperationResult::Pop(addr, bytes) => match rt.into_sgarray(bytes) {
             Ok(mut sga) => {
-                if let Some(endpoint) = addr {
-                    let saddr: SockAddrIn = {
-                        SockAddrIn {
-                            sin_family: AF_INET,
-                            sin_port: endpoint.port().into(),
-                            sin_addr: create_sin_addr(&endpoint.ip().octets()),
-                            sin_zero: [0; 8],
-                        }
-                    };
-                    sga.sga_addr = unsafe { mem::transmute::<SockAddrIn, SockAddr>(saddr) };
+                if let Some(addr) = addr {
+                    sga.sga_addr = socketaddrv4_to_sockaddr(&addr);
                 }
-                let qr_value = demi_qr_value_t { sga };
+                let qr_value: demi_qr_value_t = demi_qr_value_t { sga };
                 demi_qresult_t {
                     qr_opcode: demi_opcode_t::DEMI_OPC_POP,
                     qr_qd: qd.into(),
--- src/rust/pal/functions.rs
+++ src/rust/pal/functions.rs
@@ -1,32 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-#[cfg(feature = "catnip-libos")]
-const NUM_OCTETS_IN_IPV4: usize = 4;
-
-#[cfg(all(feature = "catnip-libos", target_os = "windows"))]
-use windows::Win32::Networking::WinSock::IN_ADDR;
-
-#[cfg(all(feature = "catnip-libos", target_os = "windows"))]
-use windows::Win32::Networking::WinSock::IN_ADDR_0;
-
-#[cfg(all(feature = "catnip-libos", target_os = "linux"))]
-use libc::in_addr;
-
-//======================================================================================================================
-// Windows functions
-//======================================================================================================================
-
-#[cfg(all(feature = "catnip-libos", target_os = "windows"))]
-pub fn create_sin_addr(octets: &[u8; NUM_OCTETS_IN_IPV4]) -> IN_ADDR {
-    IN_ADDR {
-        S_un: (IN_ADDR_0 {
-            // Always create a big-endian u32 from the given 4 bytes (in big-endian order), regardless of architecture.
-            S_addr: u32::from_ne_bytes(*octets),
-        }),
-    }
-}
-
 #[cfg(target_os = "windows")]
 use std::net::SocketAddrV4;
 
@@ -47,16 +21,4 @@ pub fn socketaddrv4_to_sockaddr(addr: &SocketAddrV4) -> SOCKADDR {
     sockaddr_in.sin_addr.S_un.S_addr = u32::from_be_bytes(addr.ip().octets());
     let sockaddr: SOCKADDR = unsafe { std::mem::transmute(sockaddr_in) };
     sockaddr
-}
-
-//======================================================================================================================
-// Linux functions
-//======================================================================================================================
-
-#[cfg(all(feature = "catnip-libos", target_os = "linux"))]
-pub fn create_sin_addr(octets: &[u8; NUM_OCTETS_IN_IPV4]) -> in_addr {
-    in_addr {
-        // Always create a big-endian u32 from the given 4 bytes (in big-endian order), regardless of architecture.
-        s_addr: u32::from_ne_bytes(*octets),
-    }
 }
