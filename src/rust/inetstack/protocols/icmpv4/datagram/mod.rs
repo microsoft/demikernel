82aab50dceea64f076198e4854096c71d067e268
Pedro Henrique Penna <ppenna@microsoft.com>
2023-11-27 13:52:24 -0300

    [build] Enhancement: Fix Build Warnings

--- src/rust/inetstack/protocols/icmpv4/datagram/mod.rs
+++ src/rust/inetstack/protocols/icmpv4/datagram/mod.rs
@@ -5,10 +5,7 @@ mod header;
 mod message;
 mod protocol;
 
-pub use self::{
-    header::ICMPV4_HEADER_SIZE,
-    protocol::ICMPV4_ECHO_REQUEST_MESSAGE_SIZE,
-};
+pub use self::protocol::ICMPV4_ECHO_REQUEST_MESSAGE_SIZE;
 pub use header::Icmpv4Header;
 pub use message::Icmpv4Message;
 pub use protocol::Icmpv4Type2;
