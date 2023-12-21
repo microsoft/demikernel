56664e29102bd5b388ff2134acb64a3ffe690f2c
Irene Zhang <irene.zhang@microsoft.com>
2023-12-21 15:33:01 +0000

    [pal] Enhancement: Remove unused constants

--- src/rust/pal/constants.rs
+++ src/rust/pal/constants.rs
@@ -37,13 +37,4 @@ pub const AF_INET: libc::sa_family_t = libc::AF_INET as u16;
 pub const AF_INET6: libc::sa_family_t = libc::AF_INET6 as u16;
 
 #[cfg(target_os = "linux")]
-pub const AF_INET_VALUE: i32 = AF_INET as i32;
-
-#[cfg(target_os = "linux")]
-pub const SOCK_STREAM: i32 = libc::SOCK_STREAM;
-
-#[cfg(target_os = "linux")]
-pub const SOCK_DGRAM: i32 = libc::SOCK_DGRAM;
-
-#[cfg(target_os = "linux")]
 pub const SOMAXCONN: i32 = libc::SOMAXCONN;
