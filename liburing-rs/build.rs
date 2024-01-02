a408e796d9accbc20185fdf82d61bc920fabb991
Pedro Henrique Penna <ppenna@microsoft.com>
2024-01-02 14:22:01 -0300

    [liburing-rs] Enhancement: Fixing Formatting

--- liburing-rs/build.rs
+++ liburing-rs/build.rs
@@ -11,9 +11,9 @@ extern crate bindgen;
 // Imports
 //==============================================================================
 
-use ::bindgen::{Bindings, Builder};
-use ::cc::Build;
-use ::std::{
+use bindgen::{Bindings, Builder};
+use cc::Build;
+use std::{
     env,
     path::{Path, PathBuf},
     process::Command,
