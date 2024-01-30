7142ec6aefe9dca74e3966172930430bbaf79344
Irene Zhang <irene.zhang@microsoft.com>
2024-01-30 17:27:47 +0000

    [runtime] Enhancement: Unify watched value

--- src/rust/collections/async_value.rs
+++ src/rust/collections/async_value.rs
@@ -6,12 +6,29 @@
 //======================================================================================================================
 
 use crate::runtime::{
+    conditional_yield_until,
+    conditional_yield_with_timeout,
     fail::Fail,
-    scheduler::{
-        Yielder,
-        YielderHandle,
+    SharedConditionVariable,
+    SharedObject,
+};
+use ::std::{
+    fmt,
+    ops::{
+        Deref,
+        DerefMut,
+    },
+    time::{
+        Duration,
+        Instant,
     },
 };
+
+//======================================================================================================================
+// Constants
+//======================================================================================================================
+
+const DEFAULT_TIMEOUT: Duration = Duration::from_secs(5);
 
 //======================================================================================================================
 // Structures
@@ -20,55 +37,86 @@ use crate::runtime::{
 /// This data structure implements single result that can be asynchronously waited on and  is hooked into the
 /// Demikernel scheduler. On get, if the value is not ready, the coroutine will yield until the value is ready.
 /// When the result is ready, the last coroutine to call get is woken.
-pub struct AsyncValue<T> {
-    value: Option<T>,
-    waiter: Option<YielderHandle>,
+#[derive(Clone)]
+pub struct AsyncValue<T: Copy + Eq> {
+    value: T,
+    cond_var: SharedConditionVariable,
+}
+
+#[derive(Clone)]
+/// Reference to an AsyncValue that is shared across coroutines.
+pub struct SharedAsyncValue<T: Copy + Eq>(SharedObject<AsyncValue<T>>);
+
+//======================================================================================================================
+// Associate Functions
+//======================================================================================================================
+
+impl<T: Copy + Eq> AsyncValue<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            value,
+            cond_var: SharedConditionVariable::default(),
+        }
+    }
+
+    pub fn set(&mut self, new_value: T) {
+        self.modify(|_| new_value)
+    }
+
+    pub fn set_without_notify(&mut self, new_value: T) {
+        self.value = new_value;
+    }
+
+    pub fn modify(&mut self, f: impl FnOnce(T) -> T) {
+        // Update the value
+        let value = f(self.value);
+        if self.value != value {
+            self.value = value;
+            self.cond_var.broadcast();
+        }
+    }
+
+    pub fn get(&self) -> T {
+        self.value
+    }
+
+    pub async fn wait_for_change(&mut self, timeout: Option<Duration>) -> Result<T, Fail> {
+        conditional_yield_with_timeout(self.cond_var.wait(), timeout.unwrap_or(DEFAULT_TIMEOUT)).await?;
+        Ok(self.value)
+    }
+
+    pub async fn wait_for_change_until(&mut self, expiry: Option<Instant>) -> Result<T, Fail> {
+        conditional_yield_until(self.cond_var.wait(), expiry).await?;
+        Ok(self.value)
+    }
+}
+
+impl<T: Copy + Eq> SharedAsyncValue<T> {
+    pub fn new(value: T) -> Self {
+        Self(SharedObject::<AsyncValue<T>>::new(AsyncValue::<T>::new(value)))
+    }
 }
 
 //======================================================================================================================
 // Associate Functions
 //======================================================================================================================
 
-impl<T: Clone> AsyncValue<T> {
-    pub fn set(&mut self, item: T) {
-        self.value = Some(item);
-        if let Some(mut yielder_handle) = self.waiter.take() {
-            yielder_handle.wake_with(Ok(()));
-        }
-    }
+impl<T: Copy + Eq> Deref for SharedAsyncValue<T> {
+    type Target = AsyncValue<T>;
 
-    pub async fn get(&mut self, yielder: Yielder) -> Result<T, Fail> {
-        match self.value.take() {
-            Some(item) => Ok(item),
-            None => {
-                let yielder_handle: YielderHandle = yielder.get_handle();
-                self.waiter = Some(yielder_handle);
-                match yielder.yield_until_wake().await {
-                    Ok(()) => match self.value.take() {
-                        Some(item) => Ok(item),
-                        None => {
-                            let cause: &str = "Spurious wake up!";
-                            warn!("get(): {}", cause);
-                            Err(Fail::new(libc::EAGAIN, cause))
-                        },
-                    },
-                    Err(e) => Err(e),
-                }
-            },
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn ready(&self) -> bool {
-        self.value.is_some()
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
     }
 }
 
-impl<T: Clone> Default for AsyncValue<T> {
-    fn default() -> Self {
-        Self {
-            value: None,
-            waiter: None,
-        }
+impl<T: Copy + Eq> DerefMut for SharedAsyncValue<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0.deref_mut()
+    }
+}
+
+impl<T: Copy + Eq + fmt::Debug> fmt::Debug for SharedAsyncValue<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "AsyncValue({:?})", self.0.value)
     }
 }
--- src/rust/inetstack/protocols/tcp/established/congestion_control/cubic.rs
+++ src/rust/inetstack/protocols/tcp/established/congestion_control/cubic.rs
@@ -17,8 +17,8 @@ use super::{
     SlowStartCongestionAvoidance,
 };
 use crate::{
+    collections::async_value::SharedAsyncValue,
     inetstack::protocols::tcp::SeqNumber,
-    runtime::watched::SharedWatchedValue,
 };
 use ::std::{
     cell::Cell,
@@ -38,8 +38,8 @@ use ::std::{
 pub struct Cubic {
     pub mss: u32, // Just for convenience, otherwise we have `as u32` or `.try_into().unwrap()` scattered everywhere...
     // Slow Start / Congestion Avoidance State.
-    pub ca_start: Cell<Instant>, // The time we started the current congestion avoidance.
-    pub cwnd: SharedWatchedValue<u32>, // Congestion window: Max number of bytes that may be in flight ot prevent congestion.
+    pub ca_start: Cell<Instant>,     // The time we started the current congestion avoidance.
+    pub cwnd: SharedAsyncValue<u32>, // Congestion window: Max number of bytes that may be in flight ot prevent congestion.
     pub fast_convergence: bool, // Should we employ the fast convergence algorithm (Only recommended if there are multiple CUBIC streams on the same network, in which case we'll cede capacity to new ones faster).
     pub initial_cwnd: u32,      // The initial value of cwnd, which gets used if the connection ever resets.
     pub last_send_time: Cell<Instant>, // The moment at which we last sent data.
@@ -51,12 +51,12 @@ pub struct Cubic {
 
     // Fast Recovery / Fast Retransmit State
     pub duplicate_ack_count: Cell<u32>, // The number of consecutive duplicate ACKs we've received.
-    pub fast_retransmit_now: SharedWatchedValue<bool>, // Flag to cause the retransmitter to retransmit a segment now.
+    pub fast_retransmit_now: SharedAsyncValue<bool>, // Flag to cause the retransmitter to retransmit a segment now.
     pub in_fast_recovery: Cell<bool>,   // Are we currently in the `fast recovery` algorithm.
     pub prev_ack_seq_no: Cell<SeqNumber>, // The previous highest ACK sequence number.
     pub recover: Cell<SeqNumber>, // If we receive dup ACKs with sequence numbers greater than this we'll attempt fast recovery.
 
-    pub limited_transmit_cwnd_increase: SharedWatchedValue<u32>, // The amount by which cwnd should be increased due to the limited transit algorithm.
+    pub limited_transmit_cwnd_increase: SharedAsyncValue<u32>, // The amount by which cwnd should be increased due to the limited transit algorithm.
 }
 
 impl CongestionControl for Cubic {
@@ -76,7 +76,7 @@ impl CongestionControl for Cubic {
             mss,
             // Slow Start / Congestion Avoidance State
             ca_start: Cell::new(Instant::now()), // Record the start time of the congestion avoidance period.
-            cwnd: SharedWatchedValue::new(initial_cwnd),
+            cwnd: SharedAsyncValue::new(initial_cwnd),
             fast_convergence,
             initial_cwnd,
             last_send_time: Cell::new(Instant::now()),
@@ -87,12 +87,12 @@ impl CongestionControl for Cubic {
             last_congestion_was_rto: Cell::new(false),
 
             in_fast_recovery: Cell::new(false),
-            fast_retransmit_now: SharedWatchedValue::new(false),
+            fast_retransmit_now: SharedAsyncValue::new(false),
             recover: Cell::new(seq_no), // Recover set to initial send sequence number according to RFC6582.
             prev_ack_seq_no: Cell::new(seq_no), // RFC6582 doesn't specify the initial value, but this seems sensible.
             duplicate_ack_count: Cell::new(0),
 
-            limited_transmit_cwnd_increase: SharedWatchedValue::new(0),
+            limited_transmit_cwnd_increase: SharedAsyncValue::new(0),
         })
     }
 }
@@ -272,7 +272,7 @@ impl Cubic {
 }
 
 impl SlowStartCongestionAvoidance for Cubic {
-    fn get_cwnd(&self) -> SharedWatchedValue<u32> {
+    fn get_cwnd(&self) -> SharedAsyncValue<u32> {
         self.cwnd.clone()
     }
 
@@ -289,8 +289,8 @@ impl SlowStartCongestionAvoidance for Cubic {
     fn on_send(&mut self, rto: Duration, num_bytes_sent: u32) {
         self.last_send_time.set(Instant::now());
         self.rtt_at_last_send.set(rto);
-        self.limited_transmit_cwnd_increase
-            .set_without_notify(self.limited_transmit_cwnd_increase.get().saturating_sub(num_bytes_sent));
+        let new_value: u32 = self.limited_transmit_cwnd_increase.get().saturating_sub(num_bytes_sent);
+        self.limited_transmit_cwnd_increase.set_without_notify(new_value);
     }
 
     fn on_ack_received(&mut self, rto: Duration, send_unacked: SeqNumber, send_next: SeqNumber, ack_seq_no: SeqNumber) {
@@ -328,7 +328,7 @@ impl FastRetransmitRecovery for Cubic {
         self.duplicate_ack_count.get()
     }
 
-    fn get_retransmit_now_flag(&self) -> SharedWatchedValue<bool> {
+    fn get_retransmit_now_flag(&self) -> SharedAsyncValue<bool> {
         self.fast_retransmit_now.clone()
     }
 
@@ -341,7 +341,7 @@ impl FastRetransmitRecovery for Cubic {
 }
 
 impl LimitedTransmit for Cubic {
-    fn get_limited_transmit_cwnd_increase(&self) -> SharedWatchedValue<u32> {
+    fn get_limited_transmit_cwnd_increase(&self) -> SharedAsyncValue<u32> {
         self.limited_transmit_cwnd_increase.clone()
     }
 }
