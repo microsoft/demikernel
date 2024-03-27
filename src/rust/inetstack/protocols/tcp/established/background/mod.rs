244178a634967a9e559c98dea9ecf2899375a183
Irene Zhang <irene.zhang@microsoft.com>
2024-03-27 00:22:54 +0000

    [inetstack] Enhancement: Add perf to background coroutine

--- src/rust/inetstack/protocols/tcp/established/background/mod.rs
+++ src/rust/inetstack/protocols/tcp/established/background/mod.rs
@@ -11,6 +11,7 @@ use self::{
     sender::sender,
 };
 use crate::{
+    async_timer,
     inetstack::protocols::tcp::established::ctrlblk::SharedControlBlock,
     runtime::{
         network::NetworkRuntime,
@@ -24,17 +25,17 @@ use ::futures::{
 };
 
 pub async fn background<N: NetworkRuntime>(cb: SharedControlBlock<N>, _dead_socket_tx: mpsc::UnboundedSender<QDesc>) {
-    let acknowledger = acknowledger(cb.clone()).fuse();
-    futures::pin_mut!(acknowledger);
+    let acknowledger = async_timer!("tcp::established::background::acknowledger", acknowledger(cb.clone())).fuse();
+    pin_mut!(acknowledger);
 
-    let retransmitter = retransmitter(cb.clone()).fuse();
-    futures::pin_mut!(retransmitter);
+    let retransmitter = async_timer!("tcp::established::background::retransmitter", retransmitter(cb.clone())).fuse();
+    pin_mut!(retransmitter);
 
-    let sender = sender(cb.clone()).fuse();
-    futures::pin_mut!(sender);
+    let sender = async_timer!("tcp::established::background::sender", sender(cb.clone())).fuse();
+    pin_mut!(sender);
 
     let mut cb2: SharedControlBlock<N> = cb.clone();
-    let receiver = cb2.poll().fuse();
+    let receiver = async_timer!("tcp::established::background::receiver", cb2.poll()).fuse();
     pin_mut!(receiver);
 
     let r = futures::select_biased! {
