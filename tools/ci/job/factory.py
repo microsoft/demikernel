350643e34949881b8f6e918bf98a012f510f347e
Pedro Henrique Penna <ppenna@microsoft.com>
2024-03-28 10:52:14 -0700

    [ci] Enhancement: Unify Windows and Linux Pipelines

---
+++ tools/ci/job/factory.py
@@ -0,0 +1,82 @@
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from ci.job.utils import wait_and_report
+import ci.job.windows as windows
+import ci.job.linux as linux
+from ci.job.generic import BaseJob
+
+# ======================================================================================================================
+# Generic Jobs
+# ======================================================================================================================
+
+
+class JobFactory:
+
+    def __init__(self, config: dict):
+        self.config = config
+
+    def checkout(self) -> BaseJob:
+        if self.config["platform"] == "windows":
+            return windows.CheckoutJobOnWindows(self.config)
+        else:
+            return linux.CheckoutJobOnLinux(self.config)
+
+    def compile(self) -> BaseJob:
+        if self.config["platform"] == "windows":
+            return windows.CompileJobOnWindows(self.config)
+        else:
+            return linux.CompileJobOnLinux(self.config)
+
+    def cleanup(self) -> BaseJob:
+        if self.config["platform"] == "windows":
+            return windows.CleanupJobOnWindows(self.config)
+        else:
+            return linux.CleanupJobOnLinux(self.config)
+
+    def unit_test(self) -> BaseJob:
+        if self.config["platform"] == "windows":
+            return windows.UnitTestJobOnWindows(self.config)
+        else:
+            return linux.UnitTestJobOnLinux(self.config)
+
+    def integration_test(self, run_mode="") -> BaseJob:
+        if self.config["platform"] == "windows":
+            raise Exception("Integration tests are not supported on Windows")
+        else:
+            if self.config["libos"] == "catmem":
+                return linux.PipeIntegrationTestJobOnLinux(self.config, run_mode=run_mode)
+            else:
+                return linux.IntegrationTestJobOnLinux(self.config)
+
+    def system_test(self, test_name: str, niterations: int = 0, run_mode: str = "", nclients: int = 0, bufsize: int = 0, nrequests: int = 0, nthreads: int = 1, who_closes: str = "", scenario: str = "") -> BaseJob:
+        if self.config["platform"] == "windows":
+            raise Exception("System tests are not supported on Windows")
+        else:
+            if self.config["libos"] == "catmem":
+                if test_name == "open-close":
+                    return linux.PipeOpenTest(self.config, niterations)
+                elif test_name == "ping-pong":
+                    return linux.PipePingPongTest(self.config)
+                elif test_name == "push-pop":
+                    return linux.PipePushPopTest(self.config)
+                else:
+                    raise Exception("Invalid test name")
+            else:
+                if test_name == "tcp_echo":
+                    return linux.TcpEchoTest(self.config, run_mode, nclients, bufsize, nrequests, nthreads)
+                elif test_name == "tcp_close":
+                    return linux.TcpCloseTest(self.config, run_mode=run_mode, who_closes=who_closes, nclients=nclients)
+                elif test_name == "tcp_wait":
+                    return linux.TcpWaitTest(self.config, scenario=scenario, nclients=nclients)
+                elif test_name == "tcp_ping_pong":
+                    return linux.TcpPingPongTest(self.config)
+                elif test_name == "tcp_push_pop":
+                    return linux.TcpPushPopTest(self.config)
+                elif test_name == "udp_ping_pong":
+                    return linux.UdpPingPongTest(self.config)
+                elif test_name == "udp_push_pop":
+                    return linux.UdpPushPopTest(self.config)
+                else:
+                    raise Exception("Invalid test name")
--- tools/ci/job/windows.py
+++ tools/ci/job/windows.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 from ci.task.windows import BaseWindowsTask, CheckoutOnWindows, CompileOnWindows, RunOnWindows, CleanupOnWindows
 from ci.job.utils import wait_and_report
+from ci.job.generic import BaseJob
 
 # ======================================================================================================================
 # Remote Commands
@@ -20,6 +21,75 @@ def remote_run_windows(host: str, repository: str, is_debug: bool, target: str, 
 # ======================================================================================================================
 # Generic Jobs for Windows
 # ======================================================================================================================
+
+
+class BaseWindowsJob(BaseJob):
+    def __init__(self, config, name):
+        super().__init__(config, name)
+
+    def execute(self, serverTask: BaseWindowsTask, clientTask: BaseWindowsTask = None) -> bool:
+        return super().execute(serverTask, clientTask)
+
+
+class CheckoutJobOnWindows(BaseWindowsJob):
+
+    def __init__(self, config: dict):
+        super().__init__(config, "checkout")
+
+    def execute(self) -> bool:
+        serverTask: CheckoutOnWindows = CheckoutOnWindows(
+            super().server(), super().repository(), super().branch())
+
+        if not super().enable_nfs():
+            clientTask: CheckoutOnWindows = CheckoutOnWindows(
+                super().client(), super().repository(), super().branch())
+            return super().execute(serverTask, clientTask)
+        return super().execute(serverTask)
+
+
+class CompileJobOnWindows(BaseWindowsJob):
+
+    def __init__(self, config: dict):
+        name = "compile-{}".format("debug" if config["is_debug"] else "release")
+        super().__init__(config, name)
+
+    def execute(self) -> bool:
+        cmd: str = f"all LIBOS={super().libos()}"
+        serverTask: CompileOnWindows = CompileOnWindows(
+            super().server(), super().repository(), cmd, super().is_debug())
+
+        if not super().enable_nfs():
+            clientTask: CompileOnWindows = CompileOnWindows(
+                super().client(), super().repository(), cmd, super().is_debug())
+            return super().execute(serverTask, clientTask)
+        return super().execute(serverTask)
+
+
+class CleanupJobOnWindows(BaseWindowsJob):
+    def __init__(self, config: dict):
+        super().__init__(config, "cleanup")
+
+    def execute(self) -> bool:
+        default_branch: str = "dev"
+        serverTask: CleanupOnWindows = CleanupOnWindows(
+            super().server(), super().repository(), super().is_sudo(), default_branch)
+
+        if not super().enable_nfs():
+            clientTask: CleanupOnWindows = CleanupOnWindows(
+                super().client(), super().repository(), super().is_sudo(), default_branch)
+            return super().execute(serverTask, clientTask)
+        return super().execute(serverTask)
+
+
+class UnitTestJobOnWindows(BaseWindowsJob):
+    def __init__(self, config: dict):
+        super().__init__(config, "unit-test")
+
+    def execute(self) -> bool:
+        server_cmd: str = f"test-unit-rust LIBOS={super().libos()}"
+        serverTask: RunOnWindows = RunOnWindows(
+            super().server(), super().repository(), server_cmd, super().is_debug(), super().is_sudo(), super().config_path())
+        return super().execute(serverTask)
 
 
 def job_checkout_windows(repository: str, branch: str, server: str, client: str, enable_nfs: bool,
