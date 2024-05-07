aac92af29424d527a92e19797d1581f57faa007b
Irene Zhang <irene.zhang@microsoft.com>
2024-05-07 23:42:24 +0000

    [bindings] Enhancement: Add support for set and get sockopt

--- src/rust/pal/constants.rs
+++ src/rust/pal/constants.rs
@@ -17,6 +17,12 @@ pub const AF_INET6: WinSock::ADDRESS_FAMILY = WinSock::AF_INET6;
 #[cfg(target_os = "windows")]
 pub const SOMAXCONN: i32 = WinSock::SOMAXCONN as i32;
 
+#[cfg(target_os = "windows")]
+pub const SOL_SOCKET: i32 = WinSock::SOL_SOCKET;
+
+#[cfg(target_os = "windows")]
+pub const SO_LINGER: i32 = WinSock::SO_LINGER;
+
 //==============================================================================
 // Linux constants
 //==============================================================================
@@ -29,3 +35,9 @@ pub const AF_INET6: libc::sa_family_t = libc::AF_INET6 as u16;
 
 #[cfg(target_os = "linux")]
 pub const SOMAXCONN: i32 = libc::SOMAXCONN;
+
+#[cfg(target_os = "linux")]
+pub const SOL_SOCKET: i32 = libc::SOL_SOCKET;
+
+#[cfg(target_os = "linux")]
+pub const SO_LINGER: i32 = libc::SO_LINGER;
