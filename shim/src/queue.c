f7b10b4a83c2078af57a0ef70cc9a2953476b65e
Pedro Henrique Penna <ppenna@microsoft.com>
2024-08-05 08:05:34 -0700

    [shim] Enhancement: Use Direct Hash for FDs

--- shim/src/queue.c
+++ shim/src/queue.c
@@ -8,111 +8,142 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct hashtable *accept_result = NULL;
-struct hashtable *pop_result = NULL;
+#define CACHE_LINE 64
+#define MAX_SOCK 1024
 
-static struct
-{
-    struct hashset *hashset_fds;
-    struct hashset *hashset_listening_fds;
+// Results from demikernel accept operations
+static uint64_t accept_result[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
+// Results from demikernel pop operations
+static uint64_t pop_result[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
 
-    // Hashes Linux EPFds -> Demikernel EPFDs.
-    struct hashtable *hashtable_epfds;
+// File descriptors for listening sockets
+static uint64_t hashset_listening_fds[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
+// File descriptors known by the demikernel
+static uint64_t hashset_fds[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
 
-    // Hashes FDs -> EPFDs.
-    struct hashtable *hashtable_fds;
-
-} queues = {NULL, NULL, NULL, NULL};
+// Hashes Linux EPFDs -> Demikernel EPFDs
+static uint64_t hashtable_epfds[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
+// Hashes FDs -> EPFDs.
+static uint64_t hashtable_fds[MAX_SOCK] __attribute__((aligned(CACHE_LINE)));
 
 void queue_man_init(void)
 {
-    queues.hashset_fds = hashset_create(10);
-    queues.hashset_listening_fds = hashset_create(10);
-    queues.hashtable_epfds = hashtable_create(10);
-    queues.hashtable_fds = hashtable_create(10);
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        hashset_fds[i] = 0;
+    }
 
-    accept_result = hashtable_create(10);
-    pop_result = hashtable_create(10);
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        hashset_listening_fds[i] = 0;
+    }
+
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        hashtable_epfds[i] = -1;
+    }
+
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        hashtable_fds[i] = -1;
+    }
+
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        accept_result[i] = 0;
+    }
+
+    for (int i = 0; i < MAX_SOCK; i++)
+    {
+        pop_result[i] = 0;
+    }
 }
 
 int queue_man_query_fd(int fd)
 {
-    return (hashset_contains(queues.hashset_fds, fd));
+    return hashset_fds[fd];
 }
 
 int queue_man_register_fd(int fd)
 {
-    return (hashset_insert(queues.hashset_fds, fd));
+    hashset_fds[fd] = 1;
+    return fd;
 }
 
 void queue_man_remove_fd(int fd)
 {
-    hashset_remove(queues.hashset_fds, fd);
+    hashset_fds[fd] = 0;
 }
 
 int queue_man_register_listen_fd(int fd)
 {
-    return (hashset_insert(queues.hashset_listening_fds, fd));
+    hashset_listening_fds[fd] = 1;
+    return 1;
 }
 
 int queue_man_is_listen_fd(int fd)
 {
-    return (hashset_contains(queues.hashset_listening_fds, fd));
+    return hashset_listening_fds[fd] == 1;
 }
 
 int queue_man_link_fd_epfd(int fd, int epfd)
 {
-    return (hashtable_insert(queues.hashtable_fds, fd, epfd));
+    hashtable_fds[fd] = epfd;
+    return 1;
 }
 
 void queue_man_unlink_fd_epfd(int fd)
 {
-    hashtable_remove(queues.hashtable_fds, fd);
+    hashtable_fds[fd] = -1;
 }
 
 int queue_man_query_fd_pollable(int fd)
 {
-    return (hashtable_get(queues.hashtable_fds, fd) != (uint64_t)-1);
+    return (hashtable_fds[fd] != (uint64_t)-1);
 }
 
 int queue_man_register_linux_epfd(int linux_epfd, int demikernel_epfd)
 {
-    return (hashtable_insert(queues.hashtable_epfds, linux_epfd, demikernel_epfd));
+    hashtable_epfds[linux_epfd] = demikernel_epfd;
+    return 1;
 }
 
 int queue_man_get_demikernel_epfd(int linux_epfd)
 {
-    return (hashtable_get(queues.hashtable_epfds, linux_epfd));
+    return (hashtable_epfds[linux_epfd]);
 }
 
 int queue_man_set_accept_result(int qd, struct demi_event *ev)
 {
-    return (hashtable_insert(accept_result, qd, (uint64_t)ev));
+    accept_result[qd] = (uint64_t)ev;
+    return 1;
 }
 
 struct demi_event *queue_man_get_accept_result(int qd)
 {
-    uint64_t ret = hashtable_get(accept_result, qd);
-    if (ret == (uint64_t)-1)
+    uint64_t ret = accept_result[qd];
+    if (ret == 0)
         return NULL;
+
     struct demi_event *ev = (void *)ret;
-    hashtable_remove(accept_result, qd);
+    accept_result[qd] = 0;
 
     return (ev);
 }
 
 int queue_man_set_pop_result(int qd, struct demi_event *ev)
 {
-    return (hashtable_insert(pop_result, qd, (uint64_t)ev));
+    pop_result[qd] = (uint64_t)ev;
+    return 1;
 }
 
 struct demi_event *queue_man_get_pop_result(int qd)
 {
-    uint64_t ret = hashtable_get(pop_result, qd);
-    if (ret == (uint64_t)-1)
+    uint64_t ret = pop_result[qd];
+    if (ret == 0)
         return NULL;
-    struct demi_event *ev = (void *)ret;
-    hashtable_remove(pop_result, qd);
 
+    struct demi_event *ev = (void *)ret;
+    pop_result[qd] = 0;
     return (ev);
 }
